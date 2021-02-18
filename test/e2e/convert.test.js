cc33f7bef77c62a18ea6144792a26864e91920b3
Carlos Veloz <carlos.veloz@carlos.veloz>
2021-02-18 13:33:42 -0600

    Test Cases for Validate method

--- test/e2e/convert.test.js
+++ test/e2e/convert.test.js
@@ -8,20 +8,20 @@ describe('Test convert function in SchemaPack', function() {
 /*read calculator file
 returns a postman collection object
 Check collection.info.name matches to 'Definitions.Name/Filename/ definitions.targetnamespace'
-Check collection.item.name matches to 'service.name'
-Check collection.item.item.name matches to 'PortType.Operation.name'
+Check collection.item.name matches to 'PortType.Operation.name'
 REQUEST
 Check default http method (POST) collection.item.item.request.method eql POST
-Check collection.item.item.request.header.key matches to 'Content-Type'
-Check collection.item.item.request.header.value matches to 'text/xml; charset=utf-8'
-Check URL collection.item.item.request.url.ras matches to 'service.port.address'
-Check Body collection.item.item.request.body matches to WSDL Body
+Check collection.item.request.header.key matches to 'Content-Type'
+Check collection.item.request.header.value matches to 'text/xml; charset=utf-8'
+Check URL collection.item.request.url.ras matches to 'service.port.address'
+Check Body collection.item.request.body matches to WSDL Body
 RESPONSE
-Check collection.item.item.response.name matches to 'PortType.Operation + Response'
-Check with Team about collection.item.item.response.originalRequest
-Check collection.item.item.response.header is an array
+Check collection.item.response.name matches to 'PortType.Operation + Response'
+Check with collection.item.response.originalRequest 
+    response fault
+Check collection.item.response.header is an array
 Check with Team, is this specified in the WSDL?
-  Check Body collection.item.item.response.body matches to WSDL Body
+  Check Body collection.item.response.body matches to WSDL Body
 */
   });
 });
--- test/e2e/validate.test.js
+++ test/e2e/validate.test.js
@@ -1,18 +1,16 @@
 const expect = require('chai').expect,
-  {
-    validate
-  } = require('../../index'),
-  fs = require('fs'),
-  {
-    SchemaPack 
-  }= require('../../lib/SchemaPack');
+  Index = require('../../index.js'),
+  empty_Path='test/data/empty.wsdl',
+  dummy_Path='test/data/dummy.wsdl',
+  simple_Path='test/data/Simple.wsdl',
+  wrong_Path='path/does/not/exist.wsdl';
 
 describe('Test validate function in SchemaPack', function() {
-  /*
+  
   it('Should get an error msg when there is no WSDL spec', function () {
-    let fileData = fs.readFileSync('test/data/dummy.wsdl', 'utf8'),
-     result = validate(fileData);
-     expect(result).to.be.an('object')
+    let schemaPack = new Index.SchemaPack({ type: 'file', data: dummy_Path }, {}),
+      result = schemaPack.validate();
+    expect(result).to.be.an('object')
       .and.to.include({
         result: false,
         reason: 'Not WSDL Specification found in your document'
@@ -20,23 +18,62 @@ describe('Test validate function in SchemaPack', function() {
   });
 
   it('Should get an error msg when file is empty', function () {
-    let fileData = fs.readFileSync('test/data/empty.wsdl', 'utf8'),
-      result = validate(fileData);
-      expect(result).to.be.an('object')
-        .and.to.include({
-          result: false,
-          reason: 'Input not provided'
-      });
+    let schemaPack = new Index.SchemaPack({ type: 'file', data: empty_Path }, {}),
+      result = schemaPack.validate();
+    expect(result).to.be.an('object')
+      .and.to.include({
+        result: false,
+        reason: 'Not WSDL Specification found in your document'
+    });
   });
-*/
+
+  it('Should get an error msg when file is not found', function() {
+    let schemaPack = new Index.SchemaPack({ type: 'file', data: wrong_Path }, {}),
+      result = schemaPack.validate();
+    expect(result).to.be.an('object')
+      .and.to.include({
+        result: false,
+        reason: 'File exist.wsdl not found' 
+    });
+  });
+
+  it('Should get an error msg when input type is other than file or string', function () {
+    let schemaPack = new Index.SchemaPack({ type: 'other', data: simple_Path }, {}),
+      result = schemaPack.validate();
+    expect(result).to.be.an('object')
+      .and.to.include({
+        result: false,
+        reason: 'Invalid input type (other). Type must be file/string.'
+    });
+  });
+
+  it('Should get an error msg when input type is undefined', function () {
+    let schemaPack = new Index.SchemaPack({ type: 'undefined', data: simple_Path }, {}),
+      result = schemaPack.validate();
+    expect(result).to.be.an('object')
+      .and.to.include({
+        result: false,
+        reason: 'Invalid input type (undefined). Type must be file/string.'
+    });
+  });
+
+  it('Should get an error msg when input data is not provided', function () {
+    let schemaPack = new Index.SchemaPack({ type: 'file' }, {}),
+      result = schemaPack.validate();
+    expect(result).to.be.an('object')
+      .and.to.include({
+        result: false,
+        reason: 'Input not provided'
+    });
+  });
+
   it('Should be successful when input is valid', function () {
-    let fileData = fs.readFileSync('test/data/Simple.wsdl', 'utf8');
-     result = validate(fileData);
+    let schemaPack = new Index.SchemaPack({ type: 'file', data: simple_Path }, {}),
+      result = schemaPack.validate();
     expect(result).to.be.an('object')
       .and.to.include({
         result: true,
         reason: 'Success'
     });
-      
   });
 });
