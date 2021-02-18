a7e73a513e7ad6ddddf11c17c5638c1b2377f720
Carlos Veloz <carlos.veloz@carlos.veloz>
2021-02-18 11:52:12 -0600

    tests

--- test/e2e/validate.test.js
+++ test/e2e/validate.test.js
@@ -2,34 +2,41 @@ const expect = require('chai').expect,
   {
     validate
   } = require('../../index'),
-  fs = require('fs');
+  fs = require('fs'),
+  {
+    SchemaPack 
+  }= require('../../lib/SchemaPack');
 
 describe('Test validate function in SchemaPack', function() {
-  
+  /*
   it('Should get an error msg when there is no WSDL spec', function () {
-    try {
-      const data = fs.readFileSync('test/data/empty.wsdl', 'utf8')
-      console.log(data)
-
-    } catch (err) {
-      console.error(err)
-    }
+    let fileData = fs.readFileSync('test/data/dummy.wsdl', 'utf8'),
+     result = validate(fileData);
+     expect(result).to.be.an('object')
+      .and.to.include({
+        result: false,
+        reason: 'Not WSDL Specification found in your document'
+    });
   });
 
-  it('Should be successful when input contains "definitions>"', function() {
-    //fs.readFileSync('test/data/Simple.wsdl');
-    try {
-      const data = fs.readFileSync('test/data/Simple.wsdl', 'utf8');
-      console.log(data);
-      let result = validate(data);
+  it('Should get an error msg when file is empty', function () {
+    let fileData = fs.readFileSync('test/data/empty.wsdl', 'utf8'),
+      result = validate(fileData);
       expect(result).to.be.an('object')
+        .and.to.include({
+          result: false,
+          reason: 'Input not provided'
+      });
+  });
+*/
+  it('Should be successful when input is valid', function () {
+    let fileData = fs.readFileSync('test/data/Simple.wsdl', 'utf8');
+     result = validate(fileData);
+    expect(result).to.be.an('object')
       .and.to.include({
         result: true,
         reason: 'Success'
     });
-    } catch (err) {
-      console.error(err)
-    }
+      
   });
-  
 });
