890467a42ce214e80638cfb5ff0a7073b252bfaf
Erik Mendoza <efernandomendoza@gmail.com>
2021-02-19 11:55:46 -0600

    Adding variables utilization for urls, substituting variables in urls

--- test/unit/WsdlToPostmanCollectionMapper.test.js
+++ test/unit/WsdlToPostmanCollectionMapper.test.js
@@ -168,7 +168,9 @@ describe('WsdlToPostmanCollectionMapper getPostmanCollection', function() {
   describe('WsdlToPostmanCollectionMapper createItemsFromOperations', function() {
     it('Should return postmanCollection items definition from wsdlObject.operationsArray', function() {
       const mapper = new WsdlToPostmanCollectionMapper(wsdlMockObject);
-      let items = mapper.createItemsFromOperations(wsdlMockObject.operationsArray),
+      let urls = mapper.getUrlFromOperations(wsdlMockObject.operationsArray),
+        urlVariables = mapper.getVariablesFromUrlList(urls),
+        items = mapper.createItemsFromOperations(wsdlMockObject.operationsArray, urlVariables),
         requests;
       expect(items).to.be.an('array');
       requests = items.map((item) => {
@@ -178,6 +180,31 @@ describe('WsdlToPostmanCollectionMapper getPostmanCollection', function() {
       requests.forEach((request) => {
         expect(request).to.be.an('object')
           .to.include.all.keys('url', 'method', 'header', 'body');
+      });
+    });
+  });
+
+  describe('WsdlToPostmanCollectionMapper getUrlFromOperations', function() {
+    it('Should an array of urls', function() {
+      const mapper = new WsdlToPostmanCollectionMapper(wsdlMockObject);
+      let urls = mapper.getUrlFromOperations(wsdlMockObject.operationsArray);
+      expect(urls).to.be.an('array');
+    });
+  });
+
+  describe('WsdlToPostmanCollectionMapper getVariablesFromUrlList', function() {
+    it('Should return an array of objects with format {key, value}', function() {
+      const mapper = new WsdlToPostmanCollectionMapper(wsdlMockObject),
+        urls = [
+          'https://www.dataaccess.com/webservicesserver/NumberConversion.wso',
+          'https://app.flowkey.com/browse/category/rock',
+          'https://heasarc.gsfc.nasa.gov/lheasoft/download.html'
+        ];
+      let variables = mapper.getVariablesFromUrlList(urls);
+      expect(variables).to.be.an('array');
+      variables.forEach((variable) => {
+        expect(variable).to.be.an('object')
+          .to.include.all.keys('key', 'value');
       });
     });
   });
