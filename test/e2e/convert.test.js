404ea5c1e59bb3559fac65ae4a3abc75256bb21b
Carlos Veloz <carlos.veloz@carlos.veloz>
2021-02-21 18:18:41 -0600

    adding e2e poc

--- test/e2e/convert.test.js
+++ test/e2e/convert.test.js
@@ -8,7 +8,8 @@ describe('Test convert function in SchemaPack', function() {
   var validWSDLsFolder = fs.readdirSync(validWSDLs);
   async.each(validWSDLsFolder, function (file, cb) {
     it('Should take a wsdl file and convert it into a Postman Collection ' + file, function () {
-      let fileContent = fs.readFileSync(validWSDLs + '/' + file, 'utf8');
+      let fileContent = fs.readFileSync(validWSDLs + '/' + file, 'utf8'),
+        opsArray = 0;
       Index.convert({ type: 'string', data: fileContent }, {}, (err, conversionResult) => {
         expect(err).to.be.null;
         expect(conversionResult.result).to.equal(true);
@@ -16,6 +17,12 @@ describe('Test convert function in SchemaPack', function() {
           expect(conversionResult.output[0].type).to.equal('collection');
           expect(conversionResult.output[0].data).to.have.property('info');
           expect(conversionResult.output[0].data).to.have.property('item');
+          opsArray = conversionResult.output[0].data.item;
+          conversionResult.output[0].data.item.forEach((item) => {
+            expect(item).to.include.all.keys('name', 'description', 'request', 'response');
+            expect(item.request).to.include.all.keys('url', 'header', 'method', 'body');
+            expect(item.response[0]).to.include.all.keys('name', 'originalRequest', 'status', 'code', 'header', 'body');
+          });
         } 
         else {
           return cb(null);
