308f5afcef19e75ed0d1d7c11bacae9ff189518c
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-02-22 17:42:19 -0600

    fix schema namespace in schema node
    when the namespace for schema is not in the root element but in the schema root element

--- test/unit/wsdl20Parser.test.js
+++ test/unit/wsdl20Parser.test.js
@@ -2,11 +2,61 @@ const expect = require('chai').expect,
   assert = require('chai').assert,
   WsdlObject = require('../../lib/WsdlObject').WsdlObject,
   {
-    Wsdl20Parser
+    Wsdl20Parser,
+    WSDL_NS_URL
   } = require('../../lib/Wsdl20Parser'),
   {
     PARSER_ATRIBUTE_NAME_PLACE_HOLDER
-  } = require('../../lib/WsdlParserCommon');
+  } = require('../../lib/WsdlParserCommon'),
+  WSDL_SAMPLE = `<?xml version="1.0" encoding="utf-8" ?>
+  <description xmlns="http://www.w3.org/ns/wsdl" 
+  targetNamespace="http://greath.example.com/2004/wsdl/resSvc" 
+  xmlns:tns="http://greath.example.com/2004/wsdl/resSvc" 
+  xmlns:ghns="http://greath.example.com/2004/schemas/resSvc" 
+  xmlns:wsoap="http://www.w3.org/ns/wsdl/soap" 
+  xmlns:soap="http://www.w3.org/2003/05/soap-envelope" 
+  xmlns:wsdlx="http://www.w3.org/ns/wsdl-extensions">
+      <documentation>
+          This document describes the GreatH Web service. Additional
+          application-level requirements for use of this service --
+          beyond what WSDL 2.0 is able to describe -- are available
+          at http://greath.example.com/2004/reservation-documentation.html
+      </documentation>
+      <types>
+          <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" 
+          targetNamespace="http://greath.example.com/2004/schemas/resSvc" 
+          xmlns="http://greath.example.com/2004/schemas/resSvc">
+              <xs:element name="checkAvailability" type="tCheckAvailability" />
+              <xs:complexType name="tCheckAvailability">
+                  <xs:sequence>
+                      <xs:element name="checkInDate" type="xs:date" />
+                      <xs:element name="checkOutDate" type="xs:date" />
+                      <xs:element name="roomType" type="xs:string" />
+                  </xs:sequence>
+              </xs:complexType>
+              <xs:element name="checkAvailabilityResponse" type="xs:double" />
+              <xs:element name="invalidDataError" type="xs:string" />
+          </xs:schema>
+      </types>
+      <interface name="reservationInterface">
+          <fault name="invalidDataFault" element="ghns:invalidDataError" />
+          <operation name="opCheckAvailability" pattern="http://www.w3.org/ns/wsdl/in-out" 
+          style="http://www.w3.org/ns/wsdl/style/iri" wsdlx:safe="true">
+              <input messageLabel="In" element="ghns:checkAvailability" />
+              <output messageLabel="Out" element="ghns:checkAvailabilityResponse" />
+              <outfault ref="tns:invalidDataFault" messageLabel="Out" />
+          </operation>
+      </interface>
+      <binding name="reservationSOAPBinding" interface="tns:reservationInterface" 
+      type="http://www.w3.org/ns/wsdl/soap" wsoap:protocol="http://www.w3.org/2003/05/soap/bindings/HTTP/">
+          <fault ref="tns:invalidDataFault" wsoap:code="soap:Sender" />
+          <operation ref="tns:opCheckAvailability" wsoap:mep="http://www.w3.org/2003/05/soap/mep/soap-response" />
+      </binding>
+      <service name="reservationService" interface="tns:reservationInterface">
+          <endpoint name="reservationEndpoint" binding="tns:reservationSOAPBinding" 
+          address="http://greath.example.com/2004/reservation" />
+      </service>
+  </description>`;
 
 describe('WSDL 2.0 parser constructor', function() {
   it('should get an object wsdl 2.0 parser', function() {
@@ -227,6 +277,47 @@ describe('WSDL 2.0 parser assignNamespaces', function() {
     expect(wsdlObject.SOAPNamespace.key).to.equal('wsoap');
     expect(wsdlObject.schemaNamespace.key).to.equal('xs');
 
+  });
+
+});
+
+
+describe('WSDL 2.0 parser getWsdlObject', function() {
+
+  it('should get an object in memory representing wsdlObject 2.0',
+    function() {
+      const parser = new Wsdl20Parser();
+      let wsdlObject = parser.getWsdlObject(WSDL_SAMPLE);
+      expect(wsdlObject).to.be.an('object');
+      expect(wsdlObject).to.have.all.keys('targetNamespace',
+        'wsdlNamespace', 'SOAPNamespace', 'HTTPNamespace',
+        'SOAP12Namespace', 'schemaNamespace',
+        'tnsNamespace', 'allNameSpaces', 'fileName',
+        'operationsArray');
+
+      expect(wsdlObject.allNameSpaces).to.be.an('array');
+      expect(wsdlObject.allNameSpaces.length).to.equal(7);
+      xmlns = wsdlObject.allNameSpaces.find((namespace) => {
+        return namespace.url === WSDL_NS_URL;
+      });
+      expect(xmlns.isDefault).to.equal(true);
+      // asserts on namespaces
+      expect(wsdlObject.targetNamespace.url).to.equal('http://greath.example.com/2004/wsdl/resSvc');
+      expect(wsdlObject.tnsNamespace.url).to.equal('http://greath.example.com/2004/wsdl/resSvc');
+      expect(wsdlObject.wsdlNamespace.key).to.equal('xmlns');
+      expect(wsdlObject.SOAPNamespace.key).to.equal('wsoap');
+      expect(wsdlObject.schemaNamespace.key).to.equal('xs');
+    });
+
+  it('should throw an error when parsedxml is null', function() {
+    try {
+      const parser = new Wsdl20Parser();
+      parser.getWsdlObject(null);
+      assert.fail('we expected an error');
+    }
+    catch (error) {
+      expect(error.message).to.equal('xmlDocumentContent must have a value');
+    }
   });
 
 });
