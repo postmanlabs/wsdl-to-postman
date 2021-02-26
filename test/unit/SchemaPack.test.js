60832ec6cca27f410cb638e615866d7f9793459b
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-02-26 10:26:54 -0600

    running with 2.0 files
    added 2.0 files

--- test/unit/SchemaPack.test.js
+++ test/unit/SchemaPack.test.js
@@ -1,10 +1,10 @@
 const expect = require('chai').expect,
   fs = require('fs'),
-  async = require('async'),
-  {
-    SchemaPack
-  } = require('../../lib/SchemaPack'),
-  validWSDLs = 'test/data/validWSDLs11';
+  async = require('async'), {
+      SchemaPack
+    } = require('../../lib/SchemaPack'),
+    validWSDLs = 'test/data/validWSDLs11',
+    validWSDLs20 = 'test/data/validWSDLs20';
 //   inputFileTemperatureHasHttp = `<wsdl:definitions
 //   xmlns:tm="http://microsoft.com/wsdl/mime/textMatching/
 //   xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/"
@@ -170,9 +170,9 @@ const expect = require('chai').expect,
 // </wsdl:definitions>
 // `
 
-describe('SchemaPack convert unit test', function() {
+describe('SchemaPack convert unit test WSDL 1.1', function() {
   var validWSDLsFolder = fs.readdirSync(validWSDLs);
-  async.each(validWSDLsFolder, function (file) {
+  async.each(validWSDLsFolder, function(file) {
     it('Should get an object representing PM Collection from ' + file, function() {
       let fileContent = fs.readFileSync(validWSDLs + '/' + file, 'utf8');
       const schemaPack = new SchemaPack({
@@ -190,21 +190,43 @@ describe('SchemaPack convert unit test', function() {
       });
     });
   });
-
-  //   it('esc 5: should get an object representing inputFileTemperatureHasHttp example PM Collection',
-  //     function() {
-  //       const schemaPack = new SchemaPack({
-  //         data: inputFileTemperatureHasHttp,
-  //         type: 'string'
-  //       }, {});
-  //       schemaPack.convert((error, result) => {
-  //         expect(error).to.be.null;
-  //         expect(result).to.be.an('object');
-  //         expect(result.output).to.be.an('array');
-  //         expect(result.output[0].data).to.be.an('object');
-  //         expect(result.output[0].type).to.equal('collection');
-  //         expect(result.output[0].data).to.be.an('object');
-  //       })
-  //     });
-
 });
+
+
+describe('SchemaPack convert unit test WSDL 2.0', function() {
+  var validWSDLsFolder = fs.readdirSync(validWSDLs20);
+  async.each(validWSDLsFolder, function(file) {
+    it('Should get an object representing PM Collection from ' + file, function() {
+      let fileContent = fs.readFileSync(validWSDLs20 + '/' + file, 'utf8');
+      const schemaPack = new SchemaPack({
+        data: fileContent,
+        type: 'string'
+      }, {});
+
+      schemaPack.convert((error, result) => {
+        expect(error).to.be.null;
+        expect(result).to.be.an('object');
+        expect(result.output).to.be.an('array');
+        expect(result.output[0].data).to.be.an('object');
+        expect(result.output[0].type).to.equal('collection');
+        expect(result.output[0].data).to.be.an('object');
+      });
+    });
+  });
+});
+
+//   it('esc 5: should get an object representing inputFileTemperatureHasHttp example PM Collection',
+//     function() {
+//       const schemaPack = new SchemaPack({
+//         data: inputFileTemperatureHasHttp,
+//         type: 'string'
+//       }, {});
+//       schemaPack.convert((error, result) => {
+//         expect(error).to.be.null;
+//         expect(result).to.be.an('object');
+//         expect(result.output).to.be.an('array');
+//         expect(result.output[0].data).to.be.an('object');
+//         expect(result.output[0].type).to.equal('collection');
+//         expect(result.output[0].data).to.be.an('object');
+//       })
+//     });
