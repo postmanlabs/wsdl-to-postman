7c026eaaa347e91956cfb6071ced0529405ca6d9
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-03-04 17:13:42 -0600

    added folder strategy option
    added folder strategy option and tests

--- test/unit/SchemaPack.test.js
+++ test/unit/SchemaPack.test.js
@@ -29,6 +29,29 @@ describe('SchemaPack convert unit test WSDL 1.1', function() {
   });
 });
 
+describe('SchemaPack convert unit test WSDL 1.1 with options', function() {
+
+  it('Should get an object representing PM Collection with two folders', function() {
+    let fileContent = fs.readFileSync(validWSDLs + '/' + 'calculator-soap11and12.wsdl', 'utf8');
+    const options = {
+        folderStrategy: 'Port/Endpoint'
+      },
+      schemaPack = new SchemaPack({
+        data: fileContent,
+        type: 'string'
+      }, options);
+
+    schemaPack.convert((error, result) => {
+      expect(error).to.be.null;
+      expect(result).to.be.an('object');
+      expect(result.output).to.be.an('array');
+      expect(result.output[0].data).to.be.an('object');
+      expect(result.output[0].type).to.equal('collection');
+      expect(result.output[0].data).to.be.an('object');
+    });
+  });
+
+});
 
 describe('SchemaPack convert unit test WSDL 2.0', function() {
   var validWSDLsFolder = fs.readdirSync(validWSDLs20);
@@ -52,18 +75,64 @@ describe('SchemaPack convert unit test WSDL 2.0', function() {
   });
 });
 
-//   it('esc 5: should get an object representing inputFileTemperatureHasHttp example PM Collection',
-//     function() {
-//       const schemaPack = new SchemaPack({
-//         data: inputFileTemperatureHasHttp,
-//         type: 'string'
-//       }, {});
-//       schemaPack.convert((error, result) => {
-//         expect(error).to.be.null;
-//         expect(result).to.be.an('object');
-//         expect(result.output).to.be.an('array');
-//         expect(result.output[0].data).to.be.an('object');
-//         expect(result.output[0].type).to.equal('collection');
-//         expect(result.output[0].data).to.be.an('object');
-//       })
-//     });
+describe('SchemaPack getOptions', function() {
+
+  it('Should return external options when called without parameters', function() {
+    const options = SchemaPack.getOptions();
+    expect(options).to.be.an('array');
+    expect(options.length).to.eq(1);
+  });
+
+  it('Should return external options when called with mode = document', function() {
+    const options = SchemaPack.getOptions('document');
+    expect(options).to.be.an('array');
+    expect(options.length).to.eq(1);
+  });
+
+  it('Should return external options when called with mode = use', function() {
+    const options = SchemaPack.getOptions('use');
+    expect(options).to.be.an('object');
+    expect(options).to.haveOwnProperty('folderStrategy');
+    expect(options.folderStrategy).to.eq('No folders');
+  });
+
+  it('Should return external options when called with criteria usage conversion', function() {
+    const options = SchemaPack.getOptions({
+      usage: ['CONVERSION']
+    });
+    expect(options).to.be.an('array');
+    expect(options.length).to.eq(1);
+  });
+
+  it('Should return external options when called with criteria usage validation', function() {
+    const options = SchemaPack.getOptions({
+      usage: ['VALIDATION']
+    });
+    expect(options).to.be.an('array');
+    expect(options.length).to.eq(0);
+  });
+
+  it('Should return external options when called with mode = use', function() {
+    const options = SchemaPack.getOptions('use', {
+      usage: ['CONVERSION']
+    });
+    expect(options).to.be.an('object');
+    expect(options).to.haveOwnProperty('folderStrategy');
+    expect(options.folderStrategy).to.eq('No folders');
+  });
+
+  it('Should return external options when called with mode = use', function() {
+    const options = SchemaPack.getOptions('document', {
+      usage: ['CONVERSION']
+    });
+    expect(options).to.be.an('array');
+    expect(options.length).to.eq(1);
+  });
+
+  it('Should return external options when called with mode = use', function() {
+    const options = SchemaPack.getOptions('document', 2);
+    expect(options).to.be.an('array');
+    expect(options.length).to.eq(1);
+  });
+
+});
