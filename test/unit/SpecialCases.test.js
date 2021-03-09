ad949b61db55aeac0073f949a6060f8f4f1dd8de
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-03-09 13:05:41 -0600

    handling not service port
    handling not finding the service port

--- test/unit/SpecialCases.test.js
+++ test/unit/SpecialCases.test.js
@@ -7,7 +7,9 @@ const expect = require('chai').expect,
   {
     DOC_HAS_NO_SERVICE_MESSAGE,
     DOC_HAS_NO_BINDIGS_MESSAGE,
-    DOC_HAS_NO_BINDIGS_OPERATIONS_MESSAGE
+    DOC_HAS_NO_BINDIGS_OPERATIONS_MESSAGE,
+    DOC_HAS_NO_SERVICE_PORT_MESSAGE
+
   } = require('../../lib/constants/messageConstants');
 
 describe('SchemaPack convert special cases WSDL 1.1', function() {
@@ -74,6 +76,27 @@ describe('SchemaPack convert special cases WSDL 1.1', function() {
       expect(result.output[0].data.info.description.content.includes(DOC_HAS_NO_BINDIGS_OPERATIONS_MESSAGE)).
       to.equal(true);
       fs.writeFileSync('coll.json', JSON.stringify(result.output[0].data))
+    });
+  });
+
+  it('Should get an object representing PM Collection from wsdl without service port ', function() {
+    let fileContent = fs.readFileSync(validWSDLs + '/NoServicesPortTag.wsdl', 'utf8');
+    const schemaPack = new SchemaPack({
+      data: fileContent,
+      type: 'string'
+    }, {});
+
+    schemaPack.convert((error, result) => {
+      expect(error).to.be.null;
+      expect(result).to.be.an('object');
+      expect(result.output).to.be.an('array');
+      expect(result.output[0].data).to.be.an('object');
+      expect(result.output[0].type).to.equal('collection');
+      expect(result.output[0].data).to.be.an('object');
+      expect(result.output[0].data.item).to.be.an('array');
+      expect(result.output[0].data.item.length).to.equal(4);
+      expect(result.output[0].data.info.description.content.includes(DOC_HAS_NO_SERVICE_PORT_MESSAGE)).
+      to.equal(true);
     });
   });
 });
