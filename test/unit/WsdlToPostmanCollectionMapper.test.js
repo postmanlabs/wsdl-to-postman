f9ab6946690a302dbd0b93c50984b99046ba34dd
Carlos Veloz <carlos.veloz@carlos.veloz>
2021-03-16 14:00:49 -0600

    fixing lint issues

--- test/unit/WsdlToPostmanCollectionMapper.test.js
+++ test/unit/WsdlToPostmanCollectionMapper.test.js
@@ -1,135 +1,137 @@
 const {
-  assert
-} = require('chai'), {
+    assert
+  } = require('chai'),
+  {
     Collection
-  } = require('postman-collection'), {
+  } = require('postman-collection'),
+  {
     WsdlToPostmanCollectionMapper,
     DEFAULT_COLLECTION_NAME
   } = require('../../lib/WsdlToPostmanCollectionMapper'),
   expect = require('chai').expect,
   wsdlMockObject = {
     operationsArray: [{
+      name: 'NumberToWords',
+      description: 'Returns the word corresponding to the positive number passed as parameter.',
+      style: 'document',
+      url: 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso',
+      input: {
+        children: [{
+          children: [],
+          name: 'ubiNum',
+          isComplex: false,
+          type: 'integer',
+          maximum: 2147483647,
+          minimum: -2147483648
+        }],
         name: 'NumberToWords',
-        description: 'Returns the word corresponding to the positive number passed as parameter.',
-        style: 'document',
-        url: 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso',
-        input: {
-          children: [{
-            children: [],
-            name: 'ubiNum',
-            isComplex: false,
-            type: 'integer',
-            maximum: 2147483647,
-            minimum: -2147483648
-          }],
-          name: 'NumberToWords',
-          isComplex: true,
-          type: 'complex',
-          namespace: 'http://www.dataaccess.com/webservicesserver/'
-        },
-        output: {
-          children: [{
-            children: [],
-            name: 'NumberToWordsResult',
-            isComplex: false,
-            type: 'string'
-          }],
-          name: 'NumberToWordsResponse',
-          isComplex: true,
-          type: 'complex',
-          namespace: 'http://www.dataaccess.com/webservicesserver/'
-        },
-        fault: {
-          children: [{
-            children: [],
-            name: 'faultcode',
-            isComplex: false,
-            type: 'string'
-          }],
-          name: 'fault',
-          isComplex: true,
-          type: 'complex',
-          namespace: ''
-        },
-        portName: 'NumberConversionSoap',
-        serviceName: 'NumberConversion',
-        method: 'POST',
-        protocol: 'soap'
+        isComplex: true,
+        type: 'complex',
+        namespace: 'http://www.dataaccess.com/webservicesserver/'
       },
-      {
+      output: {
+        children: [{
+          children: [],
+          name: 'NumberToWordsResult',
+          isComplex: false,
+          type: 'string'
+        }],
+        name: 'NumberToWordsResponse',
+        isComplex: true,
+        type: 'complex',
+        namespace: 'http://www.dataaccess.com/webservicesserver/'
+      },
+      fault: {
+        children: [{
+          children: [],
+          name: 'faultcode',
+          isComplex: false,
+          type: 'string'
+        }],
+        name: 'fault',
+        isComplex: true,
+        type: 'complex',
+        namespace: ''
+      },
+      portName: 'NumberConversionSoap',
+      serviceName: 'NumberConversion',
+      method: 'POST',
+      protocol: 'soap'
+    },
+    {
+      name: 'NumberToDollars',
+      description: 'Returns the non-zero dollar amount of the passed number.',
+      style: 'document',
+      url: 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso',
+      input: {
+        children: [{
+          children: [],
+          name: 'dNum',
+          isComplex: false,
+          type: 'number'
+        }],
         name: 'NumberToDollars',
-        description: 'Returns the non-zero dollar amount of the passed number.',
-        style: 'document',
-        url: 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso',
-        input: {
-          children: [{
-            children: [],
-            name: 'dNum',
-            isComplex: false,
-            type: 'number'
-          }],
-          name: 'NumberToDollars',
-          isComplex: true,
-          type: 'complex',
-          namespace: 'http://www.dataaccess.com/webservicesserver/'
-        },
-        output: undefined,
-        fault: undefined,
-        portName: 'NumberConversionSoap',
-        serviceName: 'NumberConversion',
-        method: 'POST',
-        protocol: 'soap'
+        isComplex: true,
+        type: 'complex',
+        namespace: 'http://www.dataaccess.com/webservicesserver/'
       },
-      {
+      output: undefined,
+      fault: undefined,
+      portName: 'NumberConversionSoap',
+      serviceName: 'NumberConversion',
+      method: 'POST',
+      protocol: 'soap'
+    },
+    {
+      name: 'NumberToWords',
+      description: 'Returns the word corresponding to the positive number passed as parameter.',
+      style: 'document',
+      url: 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso',
+      input: {
+        children: [{
+          children: [],
+          name: 'ubiNum',
+          isComplex: false,
+          type: 'integer',
+          maximum: 2147483647,
+          minimum: -2147483648
+        }],
         name: 'NumberToWords',
-        description: 'Returns the word corresponding to the positive number passed as parameter.',
-        style: 'document',
-        url: 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso',
-        input: {
-          children: [{
-            children: [],
-            name: 'ubiNum',
-            isComplex: false,
-            type: 'integer',
-            maximum: 2147483647,
-            minimum: -2147483648
-          }],
-          name: 'NumberToWords',
-          isComplex: true,
-          type: 'complex',
-          namespace: 'http://www.dataaccess.com/webservicesserver/'
-        },
-        output: undefined,
-        fault: undefined,
-        portName: 'NumberConversionSoap12',
-        serviceName: 'NumberConversion',
-        method: 'POST',
-        protocol: 'soap12'
+        isComplex: true,
+        type: 'complex',
+        namespace: 'http://www.dataaccess.com/webservicesserver/'
       },
-      {
+      output: undefined,
+      fault: undefined,
+      portName: 'NumberConversionSoap12',
+      serviceName: 'NumberConversion',
+      method: 'POST',
+      protocol: 'soap12'
+    },
+    {
+      name: 'NumberToDollars',
+      description: 'Returns the non-zero dollar amount of the passed number.',
+      style: 'document',
+      url: 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso',
+      input: {
+        children: [child = {
+          children: [],
+          name: 'dNum',
+          isComplex: false,
+          type: 'number'
+        }],
         name: 'NumberToDollars',
-        description: 'Returns the non-zero dollar amount of the passed number.',
-        style: 'document',
-        url: 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso',
-        input: {
-          children: [child = {
-            children: [],
-            name: 'dNum',
-            isComplex: false,
-            type: 'number'
-          }],
-          name: 'NumberToDollars',
-          isComplex: true,
-          type: 'complex',
-          namespace: 'http://www.dataaccess.com/webservicesserver/'
-        },
-        output: undefined,
-        fault: undefined,
-        portName: 'NumberConversionSoap12',
-        serviceName: 'NumberConversion',
-        method: 'POST',
-        protocol: 'soap12'
-      }
+        isComplex: true,
+        type: 'complex',
+        namespace: 'http://www.dataaccess.com/webservicesserver/'
+      },
+      output: undefined,
+      fault: undefined,
+      portName: 'NumberConversionSoap12',
+      serviceName: 'NumberConversion',
+      method: 'POST',
+      protocol: 'soap12'
+    }
     ],
     targetNamespace: {
       key: 'targetNamespace',
@@ -162,35 +164,35 @@ const {
       isDefault: false
     },
     allNameSpaces: [{
-        key: 'xmlns',
-        url: 'http://schemas.xmlsoap.org/wsdl/',
-        isDefault: true
-      },
-      {
-        key: 'xmlns:xs',
-        url: 'http://www.w3.org/2001/XMLSchema',
-        isDefault: false
-      },
-      {
-        key: 'xmlns:soap',
-        url: 'http://schemas.xmlsoap.org/wsdl/soap/',
-        isDefault: false
-      },
-      {
-        key: 'xmlns:soap12',
-        url: 'http://schemas.xmlsoap.org/wsdl/soap12/',
-        isDefault: false
-      },
-      {
-        key: 'xmlns:tns',
-        url: 'http://www.dataaccess.com/webservicesserver/',
-        isDefault: false
-      },
-      {
-        key: 'targetNamespace',
-        url: 'http://www.dataaccess.com/webservicesserver/',
-        isDefault: false
-      }
+      key: 'xmlns',
+      url: 'http://schemas.xmlsoap.org/wsdl/',
+      isDefault: true
+    },
+    {
+      key: 'xmlns:xs',
+      url: 'http://www.w3.org/2001/XMLSchema',
+      isDefault: false
+    },
+    {
+      key: 'xmlns:soap',
+      url: 'http://schemas.xmlsoap.org/wsdl/soap/',
+      isDefault: false
+    },
+    {
+      key: 'xmlns:soap12',
+      url: 'http://schemas.xmlsoap.org/wsdl/soap12/',
+      isDefault: false
+    },
+    {
+      key: 'xmlns:tns',
+      url: 'http://www.dataaccess.com/webservicesserver/',
+      isDefault: false
+    },
+    {
+      key: 'targetNamespace',
+      url: 'http://www.dataaccess.com/webservicesserver/',
+      isDefault: false
+    }
     ],
     fileName: undefined,
     log: {
