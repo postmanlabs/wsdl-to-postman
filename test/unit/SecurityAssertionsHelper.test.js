ba701feb30b2ad25f37216bc3f3350f64910f27a
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-03-29 16:45:34 -0600

    reading first tokens

---
+++ test/unit/SecurityAssertionsHelper.test.js
@@ -0,0 +1,122 @@
+const expect = require('chai').expect,
+  {
+    SecurityAssertionsHelper,
+  } = require('../../lib/security/SecurityAssertionsHelper'),
+  {
+    parseFromXmlToObject
+  } = require('../../lib/WsdlParserCommon');
+
+const USERNAME_TOKEN = `<wsp:Policy><sp:SupportingTokens><wsp:Policy>
+  <sp:UsernameToken sp:IncludeToken="http://docs.oasis-open.org/ws-sx/ws-securitypolicy/200702/IncludeToken/AlwaysToRecipient"
+  </wsp:Policy>
+  </sp:SupportingTokens>
+  </wsp:Policy>`,
+  USERNAME_TOKEN_NO_PASSWORD = `<wsp:Policy xmlns:wsp="..." xmlns:sp="...">  <sp:SupportingTokens>  <wsp:Policy>
+ <sp:UsernameToken sp:IncludeToken="http://docs.oasis-open.org/ws-sx/ws-securitypolicy/200702/IncludeToken/AlwaysToRecipient">
+    <wsp:Policy><sp:NoPassword/> </wsp:Policy></sp:UsernameToken> </wsp:Policy> </sp:SupportingTokens> </wsp:Policy>`,
+  USERNAME_TOKEN_HASH_PASSWORD = `<wsp:Policy xmlns:wsp="..." xmlns:sp="...">  <sp:SupportingTokens>  <wsp:Policy>
+    <sp:UsernameToken sp:IncludeToken="http://docs.oasis-open.org/ws-sx/ws-securitypolicy/200702/IncludeToken/AlwaysToRecipient">
+       <wsp:Policy><sp:HashPassword/> </wsp:Policy></sp:UsernameToken> </wsp:Policy> </sp:SupportingTokens> </wsp:Policy>`,
+  USERNAME_TOKEN_SSL = `<wsp:Policy xmlns:wsp="..." xmlns:sp="...">
+  <sp:TransportBinding>
+   <wsp:Policy>
+    <sp:TransportToken>
+    <wsp:Policy>
+    <sp:HttpsToken/>
+    </wsp:Policy>
+  </sp:TransportToken>
+   <sp:AlgorithmSuite>
+     <wsp:Policy>
+       <sp:Basic256/>
+      </wsp:Policy>
+   </sp:AlgorithmSuite>
+   <sp:Layout>
+     <wsp:Policy>
+       <sp:Strict/>
+      </wsp:Policy>
+     </sp:Layout>
+     <sp:IncludeTimestamp/>
+    </wsp:Policy>
+  </sp:TransportBinding>
+  <sp:SupportingTokens>
+    <wsp:Policy>
+       <sp:UsernameToken/>
+     </wsp:Policy>
+   </sp:SupportingTokens>
+  </wsp:Policy>`,
+  SAML_TOKEN_SUPPORTING = `<wsp:Policy>
+  <sp:SupportingTokens>
+    <wsp:Policy> 
+      <sp:SamlToken sp:IncludeToken=
+      "http://docs.oasis-open.org/ws-sx/ws-securitypolicy/200702/IncludeToken/AlwaysToRecipient">
+   <wsp:Policy>  
+   <sp:WssSamlV11Token10/>  
+   </wsp:Policy>
+   </sp:SamlToken>
+   </wsp:Policy> 
+   </sp:SupportingTokens>
+   </wsp:Policy>`;
+
+describe('SecurityAssertionsHelper getSecurityAssertions', function() {
+  it('should get an object indicating username password normal mode is used', function() {
+    const securityAssertionsHelper = new SecurityAssertionsHelper(),
+      parsedXml = parseFromXmlToObject(USERNAME_TOKEN);
+    securityAssertions = securityAssertionsHelper.getSecurityAssertions([parsedXml]);
+    expect(securityAssertions).to.be.an('object');
+    expect(securityAssertions[1]).to.be.an('array');
+    expect(securityAssertions[1][0]).to.be.an('object');
+    expect(securityAssertions[1][0].passwordType).to.equal('Normal');
+  });
+
+  it('should get an object indicating username no password mode is used', function() {
+    const securityAssertionsHelper = new SecurityAssertionsHelper(),
+      parsedXml = parseFromXmlToObject(USERNAME_TOKEN_NO_PASSWORD);
+    securityAssertions = securityAssertionsHelper.getSecurityAssertions([parsedXml]);
+    expect(securityAssertions).to.be.an('object');
+    expect(securityAssertions[1]).to.be.an('array');
+    expect(securityAssertions[1][0]).to.be.an('object');
+    expect(securityAssertions[1][0].passwordType).to.equal('NoPassword');
+    expect(securityAssertions[1][0].includeToken).to.equal('http://docs.oasis-open.org/ws-sx/ws-securitypolicy/200702/IncludeToken/AlwaysToRecipient');
+  });
+
+  it('should get an object indicating username hash password mode is used', function() {
+    const securityAssertionsHelper = new SecurityAssertionsHelper(),
+      parsedXml = parseFromXmlToObject(USERNAME_TOKEN_HASH_PASSWORD);
+    securityAssertions = securityAssertionsHelper.getSecurityAssertions([parsedXml]);
+    expect(securityAssertions).to.be.an('object');
+    expect(securityAssertions[1]).to.be.an('array');
+    expect(securityAssertions[1][0]).to.be.an('object');
+    expect(securityAssertions[1][0].passwordType).to.equal('HashPassword');
+    expect(securityAssertions[1][0].includeToken).to.equal('http://docs.oasis-open.org/ws-sx/ws-securitypolicy/200702/IncludeToken/AlwaysToRecipient');
+
+  });
+
+  it('should get an object indicating username and ssl password mode is used', function() {
+    const securityAssertionsHelper = new SecurityAssertionsHelper(),
+      parsedXml = parseFromXmlToObject(USERNAME_TOKEN_SSL);
+    securityAssertions = securityAssertionsHelper.getSecurityAssertions([parsedXml]);
+    expect(securityAssertions).to.be.an('object');
+    expect(securityAssertions[1]).to.be.an('array');
+    expect(securityAssertions[1][0]).to.be.an('object');
+    expect(securityAssertions[1][0].transportToken).to.equal('HttpsToken');
+    expect(securityAssertions[1][0].algorithmSuite).to.equal('Basic256');
+    expect(securityAssertions[1][0].layout).to.equal('Strict');
+    expect(securityAssertions[1][0].includeTimestamp).to.equal(true);
+    expect(securityAssertions[1][1]).to.be.an('object');
+    expect(securityAssertions[1][1].passwordType).to.equal('Normal');
+
+  });
+
+  it('should get an object indicating suporting saml token', function() {
+    const securityAssertionsHelper = new SecurityAssertionsHelper(),
+      parsedXml = parseFromXmlToObject(SAML_TOKEN_SUPPORTING);
+    securityAssertions = securityAssertionsHelper.getSecurityAssertions([parsedXml]);
+    expect(securityAssertions).to.be.an('object');
+    expect(securityAssertions[1]).to.be.an('array');
+    expect(securityAssertions[1][0]).to.be.an('object');
+    expect(securityAssertions[1][0].tokenVersion).to.equal('WssSamlV11Token10');
+    expect(securityAssertions[1][0].includeToken).to.equal('http://docs.oasis-open.org/ws-sx/ws-securitypolicy/200702/IncludeToken/AlwaysToRecipient');
+
+  });
+
+});
