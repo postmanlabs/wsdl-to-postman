aa6f3247960a03b93da60b94a0436a47a3070c71
Carlos Veloz <carlos.veloz@carlos.veloz>
2021-04-19 12:44:48 -0500

    negative scenarios

--- test/e2e/validateTransactions.test.js
+++ test/e2e/validateTransactions.test.js
@@ -1,21 +1,42 @@
 var expect = require('chai').expect,
-    {
-        SchemaPack
-    } = require('../../lib/SchemaPack'),
-    fs = require('fs'),
-    path = require('path'),
-    VALID_WSDL_PATH = 'test/data/validWSDLs11/numberConvertion.wsdl',
-    numberToWordsCollectionItems = require('./../data/transactionsValidation/numberToWordsCollectionItems.json'),
-    numberToWordsWSDLObject = require('./../data/transactionsValidation/wsdlObjects/numberToWords'),
-    options = {};
+  {
+    SchemaPack
+  } = require('../../lib/SchemaPack'),
+  fs = require('fs'),
+  path = require('path'),
+  VALID_WSDL_PATH = 'test/data/validWSDLs11/numberConvertion.wsdl',
+  numberToWordsCollectionItems = require('./../data/transactionsValidation/numberToWordsCollectionItems.json'),
+  numberToWordsWSDLObject = require('./../data/transactionsValidation/wsdlObjects/numberToWords'),
+  options = {},
+  notIdCollectionItems = require('./../data/transactionsValidation/numberToWordsCollNoIDs.json'),
+  nullRequestCollectionItems = require('./../data/transactionsValidation/numberToWordsCollNullRequests.json'),
+  emptyRequestCollectionItems = require('./../data/transactionsValidation/numberToWordsCollEmptyRequests.json');
 
 describe('Test validate Transactions method in SchemaPack', function() {
-    it('Should compare the PM Collection generated against the WSDL Object', function() {
-        const schemaPack = new SchemaPack({
-            type: 'file', data: VALID_WSDL_PATH
-        }, options);
-        // let schemaPack = new Converter.SchemaPack({ type: 'file', data: VALID_WSDL_PATH }, options);
-        schemaPack.validateTransactions(numberToWordsCollectionItems, schemaPack.wsdlObject);
-        console.log(schemaPack.validationResult);
+  it('Should return an error when there is not Id Collection', function() {
+    const schemaPack = new SchemaPack({
+      type: 'file', data: VALID_WSDL_PATH
+    }, options);
+    schemaPack.validateTransactions(notIdCollectionItems, (error) => {
+      expect(error.message).to.equal('Required field is null, empty or undefined');
     });
+  });
+
+  it('Should return an error when the Requests are null in the Collection', function () {
+    const schemaPack = new SchemaPack({
+      type: 'file', data: VALID_WSDL_PATH
+    }, options);
+    schemaPack.validateTransactions(nullRequestCollectionItems, (error) => {
+      expect(error.message).to.equal('Invalid syntax provided for requestList');
+    });
+  });
+
+  it('Should return an error when the Requests are empty in the Collection', function () {
+    const schemaPack = new SchemaPack({
+      type: 'file', data: VALID_WSDL_PATH
+    }, options);
+    schemaPack.validateTransactions(emptyRequestCollectionItems, (error) => {
+      expect(error.message).to.equal('Required field is null, empty or undefined');
+    });
+  });
 });
