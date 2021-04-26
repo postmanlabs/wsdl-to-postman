1612ddb4913db818127f885798b4f4e725dbe654
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-04-26 13:11:20 -0500

    lint fixes return only string xpath
    return only the xpath string

--- test/unit/wsdl20Parser.test.js
+++ test/unit/wsdl20Parser.test.js
@@ -860,7 +860,7 @@ describe('WSDL 2.0 parser assignOperations', function () {
         portName: 'SayHelloHttpSoap11Endpoint',
         serviceName: 'SayHello',
         xpathInfo: {
-          xpath: '//description//binding[@name="SayHelloSoap11Binding"]//operation[@name="hi"]',
+          xpath: '//description//binding[@name="SayHelloSoap11Binding"]//operation[@ref="tns:hi"]',
           wsdlNamespaceUrl: 'http://www.w3.org/ns/wsdl'
         }
       });
@@ -874,7 +874,7 @@ describe('WSDL 2.0 parser assignOperations', function () {
         portName: 'SayHelloHttpSoap12Endpoint',
         serviceName: 'SayHello',
         xpathInfo: {
-          xpath: '//description//binding[@name="SayHelloSoap12Binding"]//operation[@name="hi"]',
+          xpath: '//description//binding[@name="SayHelloSoap12Binding"]//operation[@ref="tns:hi"]',
           wsdlNamespaceUrl: 'http://www.w3.org/ns/wsdl'
         }
       });
@@ -888,7 +888,7 @@ describe('WSDL 2.0 parser assignOperations', function () {
         portName: 'SayHelloHttpEndpoint',
         serviceName: 'SayHello',
         xpathInfo: {
-          xpath: '//description//binding[@name="SayHelloHttpBinding"]//operation[@name="hi"]',
+          xpath: '//description//binding[@name="SayHelloHttpBinding"]//operation[@ref="tns:hi"]',
           wsdlNamespaceUrl: 'http://www.w3.org/ns/wsdl'
         }
       });
