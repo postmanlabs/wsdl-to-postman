7c3ef577e4b32768aed4e5f02044fac6d77bc0c6
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-06-03 13:07:55 -0500

    added scenario no imports in file
    no imports in file should not throw error

--- test/unit/SchemaPack.test.js
+++ test/unit/SchemaPack.test.js
@@ -381,4 +381,120 @@ describe('merge and validate', function () {
       }
     });
   });
+
+  it('Should create collection from folder having one root file for browser ex 2', function () {
+    let folderPath = path.join(__dirname, SEPARATED_FILES, '/counting'),
+      files = [],
+      array = [
+        { fileName: folderPath + '/CountingCategoryData.xsd' },
+        { fileName: folderPath + '/CountingCategoryService.wsdl' }
+      ];
+
+    array.forEach((item) => {
+      files.push({
+        content: fs.readFileSync(item.fileName, 'utf8'),
+        fileName: item.fileName
+      });
+    });
+
+    const schemaPack = new SchemaPack({ type: 'folder', data: files }, {});
+
+    schemaPack.mergeAndValidate((err, status) => {
+      if (err) {
+        expect.fail(null, null, err);
+      }
+      if (status.result) {
+        schemaPack.convert((error, result) => {
+          if (error) {
+            expect.fail(null, null, err);
+          }
+          expect(result.result).to.equal(true);
+          expect(result.output.length).to.equal(1);
+          expect(result.output[0].type).to.have.equal('collection');
+          expect(result.output[0].data).to.have.property('info');
+          expect(result.output[0].data).to.have.property('item');
+        });
+      }
+      else {
+        expect.fail(null, null, status.reason);
+      }
+    });
+  });
+
+  it('Should create collection from folder having only one file for browser', function () {
+    let folderPath = path.join(__dirname, SEPARATED_FILES, '/W3Example'),
+      files = [],
+      array = [ 
+        { fileName: folderPath + '/output.wsdl' }
+      ];
+
+    array.forEach((item) => {
+      files.push({
+        content: fs.readFileSync(item.fileName, 'utf8'),
+        fileName: item.fileName
+      });
+    });
+
+    const schemaPack = new SchemaPack({ type: 'folder', data: files }, {});
+
+    schemaPack.mergeAndValidate((err, status) => {
+      if (err) {
+        expect.fail(null, null, err);
+      }
+      if (status.result) {
+        schemaPack.convert((error, result) => {
+          if (error) {
+            expect.fail(null, null, err);
+          }
+          expect(result.result).to.equal(true);
+          expect(result.output.length).to.equal(1);
+          expect(result.output[0].type).to.have.equal('collection');
+          expect(result.output[0].data).to.have.property('info');
+          expect(result.output[0].data).to.have.property('item');
+        });
+      }
+      else {
+        expect.fail(null, null, status.reason);
+      }
+    });
+  });
+
+  it('Should create collection from folder having only one file no imports for browser', function () {
+    let folderPath = path.join(__dirname, SEPARATED_FILES, '/W3Example'),
+      files = [],
+      array = [ 
+        { fileName: folderPath + '/outputNoImports.wsdl' }
+      ];
+
+    array.forEach((item) => {
+      files.push({
+        content: fs.readFileSync(item.fileName, 'utf8'),
+        fileName: item.fileName
+      });
+    });
+
+    const schemaPack = new SchemaPack({ type: 'folder', data: files }, {});
+
+    schemaPack.mergeAndValidate((err, status) => {
+      if (err) {
+        expect.fail(null, null, err);
+      }
+      if (status.result) {
+        schemaPack.convert((error, result) => {
+          if (error) {
+            expect.fail(null, null, err);
+          }
+          expect(result.result).to.equal(true);
+          expect(result.output.length).to.equal(1);
+          expect(result.output[0].type).to.have.equal('collection');
+          expect(result.output[0].data).to.have.property('info');
+          expect(result.output[0].data).to.have.property('item');
+        });
+      }
+      else {
+        expect.fail(null, null, status.reason);
+      }
+    });
+  });
+
 });
