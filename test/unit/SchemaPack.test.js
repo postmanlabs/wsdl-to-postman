d116275afa62e14bbe2284f316bd713fa27e5006
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-06-03 11:41:40 -0500

    merge and convert
    merging and convert

--- test/unit/SchemaPack.test.js
+++ test/unit/SchemaPack.test.js
@@ -4,9 +4,11 @@ const expect = require('chai').expect,
   } = require('../../lib/SchemaPack'),
   validWSDLs = 'test/data/validWSDLs11',
   validWSDLs20 = 'test/data/validWSDLs20',
+  SEPARATED_FILES = '../data/separatedFiles',
   fs = require('fs'),
   getAllTransactions = require('../../lib/utils/getAllTransactions').getAllTransactions,
   async = require('async'),
+  path = require('path'),
   optionIds = [
     'folderStrategy',
     'validateHeader',
@@ -335,5 +337,49 @@ describe('getMetaData method', function () {
       expect(error).to.eq(null);
       expect(result.reason).to.eq('Input not provided');
     });
+  });
+});
+
+describe('merge and validate',function(){
+
+  it('Should create collection from folder having one root file for browser', function ( ) {
+      let folderPath = path.join(__dirname, SEPARATED_FILES, '/W3Example'),
+          files = [],
+          array = [
+              { fileName: folderPath + '/stockquote.xsd' },
+              { fileName: folderPath + '/stockquote.wsdl' },
+              { fileName: folderPath + '/stockquoteservice.wsdl' }
+          ];
+
+      array.forEach((item) => {
+          files.push({
+              content: fs.readFileSync(item.fileName, 'utf8'),
+              fileName: item.fileName
+          });
+      });
+
+      const schemaPack = new SchemaPack({ type: 'folder', data: files }, {});
+
+      schemaPack.mergeAndValidate((err, status) => {
+          if (err) {
+              expect.fail(null, null, err);
+          }
+          if (status.result) {
+            schemaPack.convert((error, result) => {
+                  if (error) {
+                      expect.fail(null, null, err);
+                  }
+                  expect(result.result).to.equal(true);
+                  expect(result.output.length).to.equal(1);
+                  expect(result.output[0].type).to.have.equal('collection');
+                  expect(result.output[0].data).to.have.property('info');
+                  expect(result.output[0].data).to.have.property('item');
+                  
+              });
+          }
+          else {
+              expect.fail(null, null, status.reason);
+          }
+      });
   });
 });
