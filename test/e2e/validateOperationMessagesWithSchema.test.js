d7241a3897feb8735e3be0015ea4f21d6d7fd113
Carlos Veloz <carlos.veloz@carlos.veloz>
2021-06-21 14:32:47 -0500

    fixing error with wikipedia file

--- test/e2e/validateOperationMessagesWithSchema.test.js
+++ test/e2e/validateOperationMessagesWithSchema.test.js
@@ -14,7 +14,6 @@ const fs = require('fs'),
   } = require('../../lib/XMLParser');
 
 describe('Validating wsdlObject bodyMessages using validateOperationMessagesWithSchema function', function () {
-  /*
   const WSDLsFiles = fs.readdirSync(validWSDLs),
     WSDLFilesv20 = fs.readdirSync(validWSDLs20);
   WSDLsFiles.forEach((file) => {
@@ -33,12 +32,11 @@ describe('Validating wsdlObject bodyMessages using validateOperationMessagesWith
       expect(errors).to.be.an('array');
       expect(errors.length).to.be.equal(0);
     });
-  });*/
+  });
 
-  //WSDLFilesv20.forEach((file) => {
-    it(`Should return an empty array when body messages match with schema WSDLs20 `, function () {
-      //const xmlDocumentContent = fs.readFileSync(validWSDLs20 + '/' + file, 'utf8'),
-      const xmlDocumentContent = fs.readFileSync('test/data/validWSDLs20/wikipedia.wsdl', 'utf8'),
+  WSDLFilesv20.forEach((file) => {
+    it(`Should return an empty array when body messages match with schema WSDLs20. ${file}`, function () {
+      const xmlDocumentContent = fs.readFileSync(validWSDLs20 + '/' + file, 'utf8'),
         factory = new ParserFactory(),
         xmlParser = new XMLParser(),
         version = factory.getWsdlVersion(xmlDocumentContent),
@@ -51,6 +49,6 @@ describe('Validating wsdlObject bodyMessages using validateOperationMessagesWith
       expect(errors).to.be.an('array');
       expect(errors.length).to.be.equal(0);
     });
-  //});
+  });
 });
 
