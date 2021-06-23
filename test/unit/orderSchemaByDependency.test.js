41147afb01163bbadf8c86737c30c61046807b9f
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-06-23 16:08:42 -0500

    correct schema ordering
    ordering schemas correctly

--- test/unit/orderSchemaByDependency.test.js
+++ test/unit/orderSchemaByDependency.test.js
@@ -7,7 +7,8 @@ const { expect } = require('chai'),
   validSchemaFolder = 'test/data/schemaTest',
   {
     getArrayFrom
-  } = require('../../lib/utils/objectUtils');
+  } = require('../../lib/utils/objectUtils'),
+  sapPurchaseSchemas = require('./../data/schemasToOrder/sapPurchase');
 
 
 describe('orderSchemasAccordingToDependencies method', function () {
@@ -77,5 +78,31 @@ describe('orderSchemasAccordingToDependencies method', function () {
     expect(ordered[0].foundSchemaTag['@_targetNamespace']).to.equal('https://someurl.data');
     expect(ordered[1].foundSchemaTag['@_targetNamespace']).to.equal('http://www.xsd2jsonschema.org/example');
     expect(ordered[1].foundSchemaTag['xs:import']['@_namespace']).to.equal('https://someurl.data');
+  });
+
+  it('should return the sapPurchaseSchemas ordered', function () {
+    let ordered = orderSchemasAccordingToDependencies(sapPurchaseSchemas, new XMLParser().attributePlaceHolder);
+
+    expect(ordered).to.be.a('Array');
+    expect(ordered.length).to.equal(17);
+
+    expect(ordered[0].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/BASIS/Global');
+    expect(ordered[1].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/AP/FO/PlannedLandedCost');
+    expect(ordered[2].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/AP/FO/FundManagement/Global');
+    expect(ordered[3].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/A1S');
+    expect(ordered[4].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/AP/FO/GrantManagement/Global');
+    expect(ordered[5].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/AP/PDI/bo');
+    expect(ordered[6].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/Common/DataTypes');
+    expect(ordered[7].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/AP/Common/GDT');
+    expect(ordered[8].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/AP/CRM/Global');
+    expect(ordered[9].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/AP/XU/SRM/Global');
+    expect(ordered[10].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/DocumentServices/Global');
+    expect(ordered[11].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/AP/IS/CodingBlock/Global');
+    expect(ordered[12].foundSchemaTag['@_targetNamespace'])
+      .to.equal('http://sap.com/xi/AP/FO/CashDiscountTerms/Global');
+    expect(ordered[13].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/AP/Common/Global');
+    expect(ordered[14].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/AP/Globalization');
+    expect(ordered[15].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/A1S/Global');
+    expect(ordered[16].foundSchemaTag['@_targetNamespace']).to.equal('http://sap.com/xi/SAPGlobal20/Global');
   });
 });
