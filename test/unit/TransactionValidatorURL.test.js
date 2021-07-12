cddcc7d4569e031f0f97f8ff2d2fa544d60d2ed0
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-07-12 16:22:05 -0500

    suggest available fixes
    add suggest available fixes in query param checks

--- test/unit/TransactionValidatorURL.test.js
+++ test/unit/TransactionValidatorURL.test.js
@@ -20,7 +20,12 @@ describe('validateQueryParams', function () {
     '=string&preDirectional=string&preQualifier=string&preType=string&preArticle=string&name=string&suffix' +
     '=string&postArticle=string&postQualifier=string&postDirectional=string&suiteType=string&suiteNumber=string&' +
     'city=string&state=string&zip=string&apiKey=string&version=string&shouldCalculateCensus=string&censusYear=string' +
-    '&shouldReturnReferenceGeometry=string';
+    '&shouldReturnReferenceGeometry=string',
+    geocodeAddressParsedURLMissing2 = 'Services/Geocode/WebService/GeocoderService_V04_01.asmx/' +
+    'GeocodeAddressParsed?number=string&numberFractional' +
+    '=string&preDirectional=string&preQualifier=string&preType=string&preArticle=string&name=string&suffix' +
+    '=string&postArticle=string&postQualifier=string&postDirectional=string&suiteType=string&suiteNumber=string&' +
+    'city=string&state=string&zip=string&apiKey=string&version=string&shouldCalculateCensus=string&censusYear=string';
 
   it('Should not validate queryparams when properties to ignore contains  QUERYPARAMS', function () {
     const options = getOptions({
@@ -89,4 +94,82 @@ describe('validateQueryParams', function () {
 
   });
 
+  it('Should get one missmatch with suggestion when a parameter is missing in request', function () {
+    const options = getOptions({
+        usage: ['VALIDATION']
+      }),
+      suggestAvailableFixesOption = options.find((option) => { return option.id === 'suggestAvailableFixes'; });
+    let optionFromOptions = {},
+      itemRequestProcessedInfo = {
+        url: geocodeAddressParsedURLMissing,
+        name: '',
+        protocol: 'http',
+        method: 'GET',
+        soapActionSegment: ''
+      },
+      transactionValidator,
+      result;
+    optionFromOptions[`${suggestAvailableFixesOption.id}`] = true;
+    transactionValidator = new TransactionValidator();
+    result = transactionValidator.validateQueryParams({
+      itemRequestProcessedInfo: itemRequestProcessedInfo,
+      operationFromWSDL: GeocodeAddressParsedWSDLOp, options: optionFromOptions
+    });
+    expect(result).to.be.an('array');
+    expect(result.length).to.equal(1);
+    expect(result[0]).to.be.an('object').and.to.deep.include({
+      property: 'QUERYPARAM',
+      transactionJsonPath: '$.request.url.query',
+      schemaJsonPath: '//definitions//binding[@name=\'GeocoderService_V04_01HttpGet\']' +
+      '//operation[@name=\'GeocodeAddressParsed\']',
+      reasonCode: 'MISSING_IN_REQUEST',
+      reason: 'The required query parameter "shouldNotStoreTransactionDetails" was not found in the transaction',
+      suggestedFix: {
+        key: 'shouldNotStoreTransactionDetails',
+        actualValue: null,
+        suggestedValue: {
+          key: 'shouldNotStoreTransactionDetails',
+          value: 'string'
+        }
+      }
+    });
+
+  });
+
+  it('Should get two missmatch when 2 parameters are missing in request', function () {
+    const itemRequestProcessedInfo = {
+      url: geocodeAddressParsedURLMissing2,
+      name: '',
+      protocol: 'http',
+      method: 'GET',
+      soapActionSegment: ''
+    };
+    let optionFromOptions = {},
+      transactionValidator,
+      result;
+    transactionValidator = new TransactionValidator();
+    result = transactionValidator.validateQueryParams({
+      itemRequestProcessedInfo: itemRequestProcessedInfo,
+      operationFromWSDL: GeocodeAddressParsedWSDLOp, options: optionFromOptions
+    });
+    expect(result).to.be.an('array');
+    expect(result.length).to.equal(2);
+    expect(result[0]).to.be.an('object').and.to.deep.include({
+      property: 'QUERYPARAM',
+      transactionJsonPath: '$.request.url.query',
+      schemaJsonPath: '//definitions//binding[@name=\'GeocoderService_V04_01HttpGet\']' +
+      '//operation[@name=\'GeocodeAddressParsed\']',
+      reasonCode: 'MISSING_IN_REQUEST',
+      reason: 'The required query parameter "shouldReturnReferenceGeometry" was not found in the transaction'
+    });
+    expect(result[1]).to.be.an('object').and.to.deep.include({
+      property: 'QUERYPARAM',
+      transactionJsonPath: '$.request.url.query',
+      schemaJsonPath: '//definitions//binding[@name=\'GeocoderService_V04_01HttpGet\']' +
+      '//operation[@name=\'GeocodeAddressParsed\']',
+      reasonCode: 'MISSING_IN_REQUEST',
+      reason: 'The required query parameter \"shouldNotStoreTransactionDetails\" was not found in the transaction',
+    });
+
+  });
 });
