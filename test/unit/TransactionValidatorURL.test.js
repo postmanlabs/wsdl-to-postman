c4c83c3216948c5bd1b033ae3cf52264852d7032
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-07-13 09:59:43 -0500

    added show misinginschemaerrors option
    added show misinginschemaerrors option

--- test/unit/TransactionValidatorURL.test.js
+++ test/unit/TransactionValidatorURL.test.js
@@ -1,4 +1,6 @@
 const GeocodeAddressParsedWSDLOp = require('./../data/transactionsValidation/wsdlObjects/GeocodeAddressParsedWSDLOp'),
+  GeocodeAddressParsedWSDLOpMissParam =
+  require('./../data/transactionsValidation/wsdlObjects/GeocodeAddressParsedWSDLOpMissParam'),
   {
     expect
   } = require('chai'),
@@ -16,16 +18,17 @@ describe('validateQueryParams', function () {
     'city=string&state=string&zip=string&apiKey=string&version=string&shouldCalculateCensus=string&censusYear=string' +
     '&shouldReturnReferenceGeometry=string&shouldNotStoreTransactionDetails=string',
     geocodeAddressParsedURLMissing = 'Services/Geocode/WebService/GeocoderService_V04_01.asmx/' +
-    'GeocodeAddressParsed?number=string&numberFractional' +
-    '=string&preDirectional=string&preQualifier=string&preType=string&preArticle=string&name=string&suffix' +
-    '=string&postArticle=string&postQualifier=string&postDirectional=string&suiteType=string&suiteNumber=string&' +
-    'city=string&state=string&zip=string&apiKey=string&version=string&shouldCalculateCensus=string&censusYear=string' +
-    '&shouldReturnReferenceGeometry=string',
+      'GeocodeAddressParsed?number=string&numberFractional' +
+      '=string&preDirectional=string&preQualifier=string&preType=string&preArticle=string&name=string&suffix' +
+      '=string&postArticle=string&postQualifier=string&postDirectional=string&suiteType=string&suiteNumber=string&' +
+      'city=string&state=string&zip=string&apiKey=string&version=string&shouldCalculateCensus=string&' +
+      'censusYear=string' +
+      '&shouldReturnReferenceGeometry=string',
     geocodeAddressParsedURLMissing2 = 'Services/Geocode/WebService/GeocoderService_V04_01.asmx/' +
-    'GeocodeAddressParsed?number=string&numberFractional' +
-    '=string&preDirectional=string&preQualifier=string&preType=string&preArticle=string&name=string&suffix' +
-    '=string&postArticle=string&postQualifier=string&postDirectional=string&suiteType=string&suiteNumber=string&' +
-    'city=string&state=string&zip=string&apiKey=string&version=string&shouldCalculateCensus=string&censusYear=string';
+      'GeocodeAddressParsed?number=string&numberFractional' +
+      '=string&preDirectional=string&preQualifier=string&preType=string&preArticle=string&name=string&suffix' +
+      '=string&postArticle=string&postQualifier=string&postDirectional=string&suiteType=string&suiteNumber=string&' +
+      'city=string&state=string&zip=string&apiKey=string&version=string&shouldCalculateCensus=string&censusYear=string';
 
   it('Should not validate queryparams when properties to ignore contains  QUERYPARAMS', function () {
     const options = getOptions({
@@ -87,7 +90,7 @@ describe('validateQueryParams', function () {
       property: 'QUERYPARAM',
       transactionJsonPath: '$.request.url.query',
       schemaJsonPath: '//definitions//binding[@name=\'GeocoderService_V04_01HttpGet\']' +
-      '//operation[@name=\'GeocodeAddressParsed\']',
+        '//operation[@name=\'GeocodeAddressParsed\']',
       reasonCode: 'MISSING_IN_REQUEST',
       reason: 'The required query parameter "shouldNotStoreTransactionDetails" was not found in the transaction'
     });
@@ -121,7 +124,7 @@ describe('validateQueryParams', function () {
       property: 'QUERYPARAM',
       transactionJsonPath: '$.request.url.query',
       schemaJsonPath: '//definitions//binding[@name=\'GeocoderService_V04_01HttpGet\']' +
-      '//operation[@name=\'GeocodeAddressParsed\']',
+        '//operation[@name=\'GeocodeAddressParsed\']',
       reasonCode: 'MISSING_IN_REQUEST',
       reason: 'The required query parameter "shouldNotStoreTransactionDetails" was not found in the transaction',
       suggestedFix: {
@@ -158,7 +161,7 @@ describe('validateQueryParams', function () {
       property: 'QUERYPARAM',
       transactionJsonPath: '$.request.url.query',
       schemaJsonPath: '//definitions//binding[@name=\'GeocoderService_V04_01HttpGet\']' +
-      '//operation[@name=\'GeocodeAddressParsed\']',
+        '//operation[@name=\'GeocodeAddressParsed\']',
       reasonCode: 'MISSING_IN_REQUEST',
       reason: 'The required query parameter "shouldReturnReferenceGeometry" was not found in the transaction'
     });
@@ -166,10 +169,40 @@ describe('validateQueryParams', function () {
       property: 'QUERYPARAM',
       transactionJsonPath: '$.request.url.query',
       schemaJsonPath: '//definitions//binding[@name=\'GeocoderService_V04_01HttpGet\']' +
-      '//operation[@name=\'GeocodeAddressParsed\']',
+        '//operation[@name=\'GeocodeAddressParsed\']',
       reasonCode: 'MISSING_IN_REQUEST',
-      reason: 'The required query parameter \"shouldNotStoreTransactionDetails\" was not found in the transaction',
+      reason: 'The required query parameter \"shouldNotStoreTransactionDetails\" was not found in the transaction'
     });
-
+  });
+  it('Should get one missmatch of missing in schema when a parameter is missing in schema', function () {
+    const options = getOptions({
+        usage: ['VALIDATION']
+      }),
+      suggestAvailableFixesOption = options.find((option) => { return option.id === 'showMissingInSchemaErrors'; });
+    let optionFromOptions = {},
+      itemRequestProcessedInfo = {
+        url: geocodeAddressParsedURL,
+        name: '',
+        protocol: 'http',
+        method: 'GET',
+        soapActionSegment: ''
+      },
+      transactionValidator,
+      result;
+    optionFromOptions[`${suggestAvailableFixesOption.id}`] = true;
+    transactionValidator = new TransactionValidator();
+    result = transactionValidator.validateQueryParams({
+      itemRequestProcessedInfo: itemRequestProcessedInfo,
+      operationFromWSDL: GeocodeAddressParsedWSDLOpMissParam, options: optionFromOptions
+    });
+    expect(result).to.be.an('array');
+    expect(result.length).to.equal(1);
+    expect(result[0]).to.be.an('object').and.to.deep.include({
+      property: 'QUERYPARAM',
+      transactionJsonPath: '$.request.url.query[0]',
+      schemaJsonPath: null,
+      reasonCode: 'MISSING_IN_SCHEMA',
+      reason: 'The query parameter number was not found in the schema'
+    });
   });
 });
