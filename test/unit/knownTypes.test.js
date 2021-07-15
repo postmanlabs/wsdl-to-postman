0f342050f7c7a44437deb66c0d7940fb3f7c8aa5
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-07-15 13:34:46 -0500

    validate data type and scenarios for body form
    validate data type and missing in request missing in schema

--- test/unit/knownTypes.test.js
+++ test/unit/knownTypes.test.js
@@ -2,7 +2,8 @@ const expect = require('chai').expect,
   assert = require('chai').assert,
   {
     getValueExample,
-    isKnownType
+    isKnownType,
+    isValidType
   } = require('../../lib/utils/knownTypes'),
   Element = require('../../lib/WSDLObject').Element;
 
@@ -311,4 +312,250 @@ describe('knownTypes isKnownType', function () {
     const exists = isKnownType('boolean');
     expect(exists).to.equal(true);
   });
+});
+
+describe('knownTypes isValidType', function () {
+
+  it('should get true when called with "integer"', function () {
+    const element = new Element();
+    element.type = 'integer';
+    let result = isValidType(2, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with 230, integer and min is 200 max is 250', function () {
+    const element = new Element();
+    element.type = 'integer';
+    element.minimum = 200;
+    element.maximum = 250;
+    let result = isValidType(230, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when when called with 200, integer and min 200 max 200', function () {
+    const element = new Element();
+    element.type = 'integer';
+    element.minimum = 200;
+    element.maximum = 200;
+    let result = isValidType(200, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with 200 and min undefined max 250', function () {
+    const element = new Element();
+    element.type = 'integer';
+    element.minimum = undefined;
+    element.maximum = 250;
+    let result = isValidType(200, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with 200, integer and max undefined min 200', function () {
+    const element = new Element();
+    element.type = 'integer';
+    element.minimum = 200;
+    element.maximum = undefined;
+    let result = isValidType(200, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with 1, integer and enum of 1,2,3', function () {
+    const element = new Element();
+    element.type = 'integer';
+    element.enum = [1, 2, 3];
+    let result = isValidType(1, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get false when called with "soup" and integer', function () {
+    const element = new Element();
+    element.type = 'integer';
+    let result = isValidType('soup', element);
+    expect(result).to.equal(false);
+  });
+
+  it('should get false when called with 9007199254740991 and integer', function () {
+    const element = new Element();
+    element.type = 'integer';
+    let result = isValidType(9007199254740991, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get false when called with 3.4 and integer', function () {
+    const element = new Element();
+    element.type = 'integer';
+    let result = isValidType(3.4, element);
+    expect(result).to.equal(false);
+  });
+
+  it('should get false when called with 280, integer and min is 200 max is 250', function () {
+    const element = new Element();
+    element.type = 'integer';
+    element.minimum = 200;
+    element.maximum = 250;
+    let result = isValidType(280, element);
+    expect(result).to.equal(false);
+  });
+
+  it('should get false when called with 180, integer and min is 200 max is 250', function () {
+    const element = new Element();
+    element.type = 'integer';
+    element.minimum = 200;
+    element.maximum = 250;
+    let result = isValidType(180, element);
+    expect(result).to.equal(false);
+  });
+
+  it('should get false when called with 12, integer and enum of 1,2,3', function () {
+    const element = new Element();
+    element.type = 'integer';
+    element.enum = [1, 2, 3];
+    let result = isValidType(12, element);
+    expect(result).to.equal(false);
+  });
+
+  it('should get true when called with number and value 2', function () {
+    const element = new Element();
+    element.type = 'number';
+    let result = isValidType(2, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with 230 number and min is 200 max is 250', function () {
+    const element = new Element();
+    element.type = 'number';
+    element.minimum = 200;
+    element.maximum = 250;
+    let result = isValidType(230, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get false when called with 3.4', function () {
+    const element = new Element();
+    element.type = 'number';
+    let result = isValidType(3.4, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with "string" element and value', function () {
+    const element = new Element();
+    element.type = 'string';
+    let result = isValidType('string', element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with "string" element and value length of 6', function () {
+    const element = new Element();
+    element.type = 'string';
+    element.minLength = 5;
+    element.maxLength = 10;
+    let result = isValidType('string', element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with "string" with no minimum and maximum 10 value length of 6', function () {
+    const element = new Element();
+    element.type = 'string';
+    element.maxLength = 10;
+    let result = isValidType('string', element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when generating with pattern and validate', function () {
+    const element = new Element();
+    element.type = 'string';
+    element.pattern = '"-?([1-9][0-9]{3,}|0[0-9]{3})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]' +
+      '|3[01])(Z|(\\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))?"';
+    let example = getValueExample(element),
+      result = isValidType(example, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with type empty', function () {
+    const element = new Element();
+    element.type = '';
+    result = isValidType('', element);
+    expect(result).to.equal(true);
+  });
+
+
+  it('should get true when called with an empty string', function () {
+    const element = new Element();
+    element.type = 'string';
+    result = isValidType('', element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with boolean and true', function () {
+    const element = new Element();
+    element.type = 'boolean';
+    result = isValidType(true, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with boolean and false', function () {
+    const element = new Element();
+    element.type = 'boolean';
+    let result = isValidType(false, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with decimal and 1.2', function () {
+    const element = new Element();
+    element.type = 'decimal';
+    let result = isValidType(1.2, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get a number between 2 and 100 when called with float', function () {
+    const element = new Element();
+    element.type = 'float';
+    let result = isValidType(1.2, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get a number between 2 and 100 when called with double', function () {
+    const element = new Element();
+    element.type = 'double';
+    let result = isValidType(1.2, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with unsignedInt and 2', function () {
+    const element = new Element();
+    element.type = 'unsignedInt';
+    let result = isValidType(2, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get false when called with unsignedInt and -2', function () {
+    const element = new Element();
+    element.type = 'unsignedInt';
+    let result = isValidType(-2, element);
+    expect(result).to.equal(false);
+  });
+
+  it('should get true when called with date and a valid generated date', function () {
+    const element = new Element();
+    element.type = 'date';
+    let example = getValueExample(element),
+      result = isValidType(example, element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with boolean and value of \'false\'', function () {
+    const element = new Element();
+    element.type = 'boolean';
+    let result = isValidType('false', element);
+    expect(result).to.equal(true);
+  });
+
+  it('should get true when called with integer and value of \'9\'', function () {
+    const element = new Element();
+    element.type = 'integer';
+    let result = isValidType('9', element);
+    expect(result).to.equal(true);
+  });
+
+
 });
---
+++ test/unit/transactionValidator/BodyFormEncoded.test.js
@@ -0,0 +1,322 @@
+const GeocodeAddressParsedWSDLOpHTTPPOST =
+  require('../../data/transactionsValidation/wsdlObjects/GeocodeAddressParsedWSDLOpHTTPPOST'),
+  GeocodeAddressParsedWSDLOpHTTPPOSTMissParam =
+    require('../../data/transactionsValidation/wsdlObjects/GeocodeAddressParsedWSDLOpHTTPPOSTMissParam'),
+  {
+    expect
+  } = require('chai'),
+  {
+    BodyFormEncodedValidator
+  } = require('../../../lib/transactionValidator/BodyFormEncodedValidator'),
+  getOptions = require('../../../lib/utils/options').getOptions;
+
+
+describe('validate BodyFormEncodedValidator', function () {
+
+  const VALID_BODY = {
+    mode: 'urlencoded',
+    urlencoded: [
+      {
+        key: 'number',
+        value: 'string',
+      },
+      {
+        key: 'numberFractional',
+        value: 'string',
+      },
+      {
+        key: 'preDirectional',
+        value: 'string',
+      },
+      {
+        key: 'preQualifier',
+        value: 'string',
+      },
+      {
+        key: 'preType',
+        value: 'string',
+      },
+      {
+        key: 'preArticle',
+        value: 'string',
+      },
+      {
+        key: 'name',
+        value: 'string',
+      },
+      {
+        key: 'suffix',
+        value: 'string',
+      },
+      {
+        key: 'postArticle',
+        value: 'string',
+      },
+      {
+        key: 'postQualifier',
+        value: 'string',
+      },
+      {
+        key: 'postDirectional',
+        value: 'string',
+      },
+      {
+        key: 'suiteType',
+        value: 'string',
+      },
+      {
+        key: 'suiteNumber',
+        value: 'string',
+      },
+      {
+        key: 'city',
+        value: 'string',
+      },
+      {
+        key: 'state',
+        value: 'string',
+      },
+      {
+        key: 'zip',
+        value: 'string',
+      },
+      {
+        key: 'apiKey',
+        value: 'string',
+      },
+      {
+        key: 'version',
+        value: 'string',
+      },
+      {
+        key: 'shouldCalculateCensus',
+        value: 'string',
+      },
+      {
+        key: 'censusYear',
+        value: 'string',
+      },
+      {
+        key: 'shouldReturnReferenceGeometry',
+        value: 'string',
+      },
+      {
+        key: 'shouldNotStoreTransactionDetails',
+        value: 'string',
+      },
+    ],
+  };
+
+  const INVALID_BODY_MISS_PROPERTY = {
+    mode: 'urlencoded',
+    urlencoded: [
+      {
+        key: 'numberFractional',
+        value: 'string',
+      },
+      {
+        key: 'preDirectional',
+        value: 'string',
+      },
+      {
+        key: 'preQualifier',
+        value: 'string',
+      },
+      {
+        key: 'preType',
+        value: 'string',
+      },
+      {
+        key: 'preArticle',
+        value: 'string',
+      },
+      {
+        key: 'name',
+        value: 'string',
+      },
+      {
+        key: 'suffix',
+        value: 'string',
+      },
+      {
+        key: 'postArticle',
+        value: 'string',
+      },
+      {
+        key: 'postQualifier',
+        value: 'string',
+      },
+      {
+        key: 'postDirectional',
+        value: 'string',
+      },
+      {
+        key: 'suiteType',
+        value: 'string',
+      },
+      {
+        key: 'suiteNumber',
+        value: 'string',
+      },
+      {
+        key: 'city',
+        value: 'string',
+      },
+      {
+        key: 'state',
+        value: 'string',
+      },
+      {
+        key: 'zip',
+        value: 'string',
+      },
+      {
+        key: 'apiKey',
+        value: 'string',
+      },
+      {
+        key: 'version',
+        value: 'string',
+      },
+      {
+        key: 'shouldCalculateCensus',
+        value: 'string',
+      },
+      {
+        key: 'censusYear',
+        value: 'string',
+      },
+      {
+        key: 'shouldReturnReferenceGeometry',
+        value: 'string',
+      },
+      {
+        key: 'shouldNotStoreTransactionDetails',
+        value: 'string',
+      },
+    ],
+  };
+
+  it('Should not validate body when properties to ignore contains BODY', function () {
+    const options = getOptions({
+      usage: ['VALIDATION']
+    }),
+      validateHeaderOption = options.find((option) => { return option.id === 'validationPropertiesToIgnore'; });
+    let optionFromOptions = {},
+      paramsValidator,
+      result;
+    optionFromOptions[`${validateHeaderOption.id}`] = 'BODY';
+    paramsValidator = new BodyFormEncodedValidator();
+    result = paramsValidator.validate({
+      body: {}, operationFromWSDLL: {},
+      options: optionFromOptions,
+      isResponse: false
+    });
+    expect(result).to.be.an('array');
+    expect(result.length).to.equal(0);
+  });
+
+  it('Shouldn\'t validate body when properties to ignore contains RESPONSE_BODY and isResponse true', function () {
+    const options = getOptions({
+        usage: ['VALIDATION']
+      }),
+      validateHeaderOption = options.find((option) => { return option.id === 'validationPropertiesToIgnore'; });
+    let optionFromOptions = {},
+      paramsValidator,
+      result;
+    optionFromOptions[`${validateHeaderOption.id}`] = 'RESPONSE_BODY';
+    paramsValidator = new BodyFormEncodedValidator();
+    result = paramsValidator.validate({
+      body: {}, operationFromWSDLL: {},
+      options: optionFromOptions,
+      isResponse: true
+    });
+    expect(result).to.be.an('array');
+    expect(result.length).to.equal(0);
+  });
+
+  it('Should get one missmatch when a parameter is missing in schema', function () {
+    const options = getOptions({
+        usage: ['VALIDATION']
+      }),
+      showMissingInSchemaErrors = options.find((option) => { return option.id === 'showMissingInSchemaErrors'; });
+    let optionFromOptions = {},
+      paramsValidator,
+      result;
+    optionFromOptions[`${showMissingInSchemaErrors.id}`] = true;
+    paramsValidator = new BodyFormEncodedValidator();
+    result = paramsValidator.validate({
+      body: VALID_BODY,
+      operationFromWSDL: GeocodeAddressParsedWSDLOpHTTPPOSTMissParam, options: optionFromOptions,
+      isResponse: false
+    });
+    expect(result).to.be.an('array');
+    expect(result.length).to.equal(1);
+    expect(result[0]).to.be.an('object').and.to.deep.include({
+      property: 'BODY',
+      transactionJsonPath: '$.request.body.urlencoded[0]',
+      schemaJsonPath: null,
+      reasonCode: 'MISSING_IN_SCHEMA',
+      reason: 'The Url Encoded body param "number" was not found in the schema'
+    });
+  });
+
+  it('Should get one missmatch with suggestion when a parameter is missing in request', function () {
+    let paramsValidator,
+      result;
+    paramsValidator = new BodyFormEncodedValidator();
+    result = paramsValidator.validate({
+      body: INVALID_BODY_MISS_PROPERTY,
+      operationFromWSDL: GeocodeAddressParsedWSDLOpHTTPPOST, options: {},
+      isResponse: false
+    });
+    expect(result).to.be.an('array');
+    expect(result.length).to.equal(1);
+    expect(result[0]).to.be.an('object').and.to.deep.include({
+      property: 'BODY',
+      transactionJsonPath: '$.request.body.urlencoded',
+      schemaJsonPath: '//definitions//binding[@name=\'GeocoderService_V04_01HttpPost\']' +
+        '//operation[@name=\'GeocodeAddressParsed\']',
+      reasonCode: 'MISSING_IN_REQUEST',
+      reason: 'The Url Encoded body param "number" was not found in the transaction'
+    });
+
+  });
+
+  // it('Should get two missmatch when 2 parameters are missing in request', function () {
+  //   const itemRequestProcessedInfo = {
+  //     fullURL: geocodeAddressParsedURLMissing2,
+  //     name: '',
+  //     protocol: 'http',
+  //     method: 'GET',
+  //     soapActionSegment: ''
+  //   };
+  //   let optionFromOptions = {},
+  //     paramsValidator,
+  //     result;
+  //   paramsValidator = new QueryParamsValidator();
+  //   result = paramsValidator.validate({
+  //     itemRequestProcessedInfo: itemRequestProcessedInfo,
+  //     operationFromWSDL: GeocodeAddressParsedWSDLOp, options: optionFromOptions
+  //   });
+  //   expect(result).to.be.an('array');
+  //   expect(result.length).to.equal(2);
+  //   expect(result[0]).to.be.an('object').and.to.deep.include({
+  //     property: 'QUERYPARAM',
+  //     transactionJsonPath: '$.request.url.query',
+  //     schemaJsonPath: '//definitions//binding[@name=\'GeocoderService_V04_01HttpGet\']' +
+  //       '//operation[@name=\'GeocodeAddressParsed\']',
+  //     reasonCode: 'MISSING_IN_REQUEST',
+  //     reason: 'The required query parameter "shouldReturnReferenceGeometry" was not found in the transaction'
+  //   });
+  //   expect(result[1]).to.be.an('object').and.to.deep.include({
+  //     property: 'QUERYPARAM',
+  //     transactionJsonPath: '$.request.url.query',
+  //     schemaJsonPath: '//definitions//binding[@name=\'GeocoderService_V04_01HttpGet\']' +
+  //       '//operation[@name=\'GeocodeAddressParsed\']',
+  //     reasonCode: 'MISSING_IN_REQUEST',
+  //     reason: 'The required query parameter \"shouldNotStoreTransactionDetails\" was not found in the transaction'
+  //   });
+  // });
+
+});
