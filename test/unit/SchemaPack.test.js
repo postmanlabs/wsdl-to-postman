c46c4e6b22e32dee634d846d9ffb211fa08d4920
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-08-05 16:43:04 -0500

    issue merging files
    wip merging files and properties

--- test/unit/SchemaPack.test.js
+++ test/unit/SchemaPack.test.js
@@ -558,4 +558,35 @@ describe('merge and validate', function () {
       }
     });
   });
+
+  it('Should create collection from folder send only file info principal prefix', function (done) {
+    let folderPath = path.join(__dirname, SEPARATED_FILES, '/W3ExamplePrincipalPrefix'),
+      array = [
+        { fileName: folderPath + '/goodService.wsdl' },
+        { fileName: folderPath + '/goodService-common.wsdl' }
+      ];
+    const schemaPack = new SchemaPack({ type: 'folder', data: array }, {});
+    schemaPack.mergeAndValidate((err, status) => {
+      if (err) {
+        expect.fail(null, null, err);
+      }
+      if (status.result) {
+        schemaPack.convert((error, result) => {
+          if (error) {
+            expect.fail(null, null, err);
+          }
+          expect(result.result).to.equal(true);
+          expect(result.output.length).to.equal(1);
+          expect(result.output[0].type).to.have.equal('collection');
+          expect(result.output[0].data).to.have.property('info');
+          expect(result.output[0].data).to.have.property('item');
+          expect(result.output[0].data.info.name).to.equal('GoodServicePortal');
+          done();
+        });
+      }
+      else {
+        expect.fail(null, null, status.reason);
+      }
+    });
+  });
 });
