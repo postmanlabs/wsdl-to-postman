0dbbe3a34678349bef8e8080d6edeb40db7a0aa2
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-08-23 13:10:42 -0500

    Added test with full transactions object
    Added test with full transactions object

--- test/unit/TransactionValidator.test.js
+++ test/unit/TransactionValidator.test.js
@@ -37,6 +37,9 @@ const notIdCollectionItems = require('./../data/transactionsValidation/notIdColl
    require('./../data/transactionsValidation/numberToWordsCollectionItemsInvalidXMLBody.json'),
   expectedMissingEndpointaebb36fc =
   require('./../data/transactionsValidation/resultValidation/expectedMissingEndpointaebb36fc'),
+  numberToWordsCollectionItemsNOSOAPAcHeader =
+  require('./../data/transactionsValidation/numberToWordsCollectionItemsNOSOAPAcHeader.json'),
+
   {
     assert,
     expect
@@ -840,6 +843,105 @@ describe('Validate Headers', function() {
       }
     });
   });
+
+  it('Should return missmatch when soap action header is missing',
+    function () {
+      const options = getOptions({
+          usage: ['VALIDATION']
+        }),
+        validateHeaderOption = options.find((option) => { return option.id === 'validateHeader'; });
+      let optionFromOptions = {},
+        transactionValidator,
+        result;
+      optionFromOptions[`${validateHeaderOption.id}`] = true;
+      transactionValidator = new TransactionValidator();
+      result = transactionValidator.validateTransaction(numberToWordsCollectionItemsNOSOAPAcHeader,
+        numberToWordsWSDLObject, new XMLParser(), optionFromOptions);
+      expect(result).to.be.an('object').and.to.deep.include({
+        matched: false,
+        requests: {
+          '18403328-4213-4c3e-b0e9-b21a636697c3': {
+            endpoints: [{
+              matched: true,
+              endpointMatchScore: 1,
+              endpoint: 'POST soap12 NumberToDollars',
+              mismatches: [],
+              responses: {
+                '1763f0b2-9f34-4796-a390-b94ee5c37c7c': {
+                  id: '1763f0b2-9f34-4796-a390-b94ee5c37c7c',
+                  matched: true,
+                  mismatches: []
+                }
+              }
+            }],
+            requestId: '18403328-4213-4c3e-b0e9-b21a636697c3'
+          },
+          '353e33da-1eee-41c1-8865-0f72b2e1fd10': {
+            endpoints: [{
+              matched: true,
+              endpointMatchScore: 1,
+              endpoint: 'POST soap12 NumberToWords',
+              mismatches: [],
+              responses: {
+                'c8a892b6-4b2e-4523-9cc3-fc3e08c835c4': {
+                  id: 'c8a892b6-4b2e-4523-9cc3-fc3e08c835c4',
+                  matched: true,
+                  mismatches: []
+                }
+              }
+            }],
+            requestId: '353e33da-1eee-41c1-8865-0f72b2e1fd10'
+          },
+          '395c9db6-d6f5-45a7-90f5-09f5aab4fe92': {
+            endpoints: [{
+              matched: true,
+              endpointMatchScore: 1,
+              endpoint: 'POST soap NumberToDollars',
+              mismatches: [],
+              responses: {
+                '8a0c6532-84f9-45c7-838a-f4bf1a6de002': {
+                  id: '8a0c6532-84f9-45c7-838a-f4bf1a6de002',
+                  matched: true,
+                  mismatches: []
+                }
+              }
+            }],
+            requestId: '395c9db6-d6f5-45a7-90f5-09f5aab4fe92'
+          },
+          'aebb36fc-1be3-44c3-8f4a-0b5042dc17d0': {
+            endpoints: [{
+              matched: false,
+              endpointMatchScore: 1,
+              endpoint: 'POST soap NumberToWords',
+              mismatches: [
+                {
+                  property: 'HEADER',
+                  transactionJsonPath: '$.request.header',
+                  schemaJsonPath: 'schemaPathPrefix',
+                  reasonCode: 'MISSING_IN_REQUEST',
+                  reason: 'The header "SoapAction" was not found in the transaction'
+                }
+              ],
+              responses: {
+                'd36c56cf-0cf6-4273-a34d-973e842bf80f': {
+                  id: 'd36c56cf-0cf6-4273-a34d-973e842bf80f',
+                  matched: false,
+                  mismatches: [{
+                    'property': 'HEADER',
+                    'reason': 'The header "Content-Type" needs to be "text/xml" or "application/soap+xml" ' +
+                    'but we found "text/plain; charset=utf-8" instead',
+                    'reasonCode': 'INVALID_TYPE',
+                    'schemaJsonPath': 'schemaPathPrefix',
+                    'transactionJsonPath': '$.responses[d36c56cf-0cf6-4273-a34d-973e842bf80f].header[0].value'
+                  }]
+                }
+              }
+            }],
+            requestId: 'aebb36fc-1be3-44c3-8f4a-0b5042dc17d0'
+          }
+        }
+      });
+    });
 });
 
 
