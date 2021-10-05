5a23284282f15e189232430e3bcb48763566f1f1
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2021-10-05 15:45:16 -0500

    Add parserOptions as class property
    Add parserOptions as class property

--- test/unit/SchemaPack.test.js
+++ test/unit/SchemaPack.test.js
@@ -211,6 +211,26 @@ describe('SchemaPack convert unit test WSDL 1.1 with options', function () {
     });
   });
 
+  it('Should get an object representing PM Collection with tabs in message', function () {
+    let fileContent = fs.readFileSync(validWSDLs + '/calculator-soap11and12.wsdl', 'utf8');
+    const options = { folderStrategy: 'Port/Endpoint', indentCharacter: '\t' },
+      schemaPack = new SchemaPack({
+        data: fileContent,
+        type: 'string'
+      }, options);
+
+    schemaPack.convert((error, result) => {
+      expect(error).to.be.null;
+      expect(result).to.be.an('object');
+      expect(result.output).to.be.an('array');
+      expect(result.output[0].data).to.be.an('object');
+      expect(result.output[0].type).to.equal('collection');
+      expect(result.output[0].data.item).to.be.an('array');
+      expect(result.output[0].data.item.length).to.equal(2);
+      expect(result.output[0].data.item[0].item[0].request.body.raw.includes('\t')).to.equal(true);
+
+    });
+  });
 });
 
 describe('SchemaPack convert unit test WSDL 2.0', function () {
@@ -256,7 +276,7 @@ describe('SchemaPack getOptions', function () {
   it('Should return external options when called with mode = document', function () {
     const options = SchemaPack.getOptions('document');
     expect(options).to.be.an('array');
-    expect(options.length).to.eq(9);
+    expect(options.length).to.eq(10);
   });
 
   it('Should return external options when called with mode = use', function () {
@@ -298,13 +318,13 @@ describe('SchemaPack getOptions', function () {
       usage: ['CONVERSION']
     });
     expect(options).to.be.an('array');
-    expect(options.length).to.eq(3);
+    expect(options.length).to.eq(4);
   });
 
   it('Should return external options when called with mode document and usage not an object', function () {
     const options = SchemaPack.getOptions('document', 2);
     expect(options).to.be.an('array');
-    expect(options.length).to.eq(9);
+    expect(options.length).to.eq(10);
   });
 
   it('Should return default empty array in validationPropertiesToIgnore', function () {
