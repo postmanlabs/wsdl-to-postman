1aafaa41501a8a5dfca6f86ffa7d73615486aeee
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-09-30 09:49:36 -0500

    filter rootfiles by version

--- test/unit/relatedFiles.test.js
+++ test/unit/relatedFiles.test.js
@@ -2,7 +2,9 @@ const { getAdjacentAndMissing,
     getReferences,
     calculatePath,
     calculatePathMissing,
-    getRelatedFiles } = require('./../../lib/relatedFiles'),
+    getRelatedFiles,
+    getRelatedFilesFromInput
+  } = require('./../../lib/relatedFiles'),
   expect = require('chai').expect,
   fs = require('fs'),
   path = require('path'),
@@ -14,6 +16,7 @@ const { getAdjacentAndMissing,
   MISSING_IMPORT = '../data/separatedFiles/missingImport',
   NESTED_FOLDERS = '../data/separatedFiles/nestedFolders',
   EXTRA_FILE = '../data/separatedFiles/wsdlfolderextrafilelocal',
+  WIKI_2_0 = '../data/separatedFiles/wiki20',
   ALL_P_SERVICE = path.join(__dirname, ALL_IMPORT_PRESENT + '/CountingCategoryService.wsdl'),
   ALL_P_DATA = path.join(__dirname, ALL_IMPORT_PRESENT + '/CountingCategoryData.xsd'),
   C_C_SERVICE = path.join(__dirname, COUNTING_SEPARATED + '/CountingCategoryService.wsdl'),
@@ -27,7 +30,9 @@ const { getAdjacentAndMissing,
   E_F_STOCK_WSDL = path.join(__dirname, EXTRA_FILE + '/stockquote.wsdl'),
   E_F_STOCK_XSD = path.join(__dirname, EXTRA_FILE + '/stockquote.xsd'),
   E_F_STOCK_SERVICE = path.join(__dirname, EXTRA_FILE + '/stockquoteservice.wsdl'),
-  E_F_STOCK_README = path.join(__dirname, EXTRA_FILE + '/README.xml');
+  E_F_STOCK_README = path.join(__dirname, EXTRA_FILE + '/README.xml'),
+  WIKI_2_0_SERVICE = path.join(__dirname, WIKI_2_0 + '/wikipedia.wsdl'),
+  WIKI_2_0_TYPES = path.join(__dirname, WIKI_2_0 + '/Types.xsd');
 
 
 describe('getReferences function', function () {
@@ -155,7 +160,7 @@ describe('getAdjacentAndMissing function', function () {
 
 describe('getRelatedFiles function ', function () {
 
-  it('should return related in a folder nested structure', function () {
+  it('should return related files from a folder nested structure', function () {
     const services = fs.readFileSync(NESTED_F_SERVICES, 'utf8'),
       types = fs.readFileSync(NESTED_F_TYPES, 'utf8'),
       rootNode = {
@@ -210,7 +215,7 @@ describe('getRelatedFiles function ', function () {
     expect(missingRelatedFiles[0].schemaLocation).to.equal('../../../common/v1/CommonData.xsd');
   });
 
-  it('should return related in a folder with files that are not wsdl', function () {
+  it('should return related files from a folder with files that are not wsdl', function () {
     const otherWsdl = fs.readFileSync(E_F_STOCK_WSDL, 'utf8'),
       types = fs.readFileSync(E_F_STOCK_XSD, 'utf8'),
       services = fs.readFileSync(E_F_STOCK_SERVICE, 'utf8'),
@@ -238,6 +243,274 @@ describe('getRelatedFiles function ', function () {
     expect(relatedFiles[0].path).to.equal('stockquote.wsdl');
     expect(relatedFiles[1].path).to.equal('stockquote.xsd');
     expect(missingRelatedFiles.length).to.equal(0);
+  });
+
+  it('should return related files version 2.0', function () {
+    const services = fs.readFileSync(WIKI_2_0_SERVICE, 'utf8'),
+      types = fs.readFileSync(WIKI_2_0_TYPES, 'utf8'),
+      rootNode = {
+        path: 'wikipedia.wsdl',
+        content: services
+      },
+      inputData = [
+        {
+          path: 'Types.xsd',
+          content: types
+        }
+      ],
+      { relatedFiles, missingRelatedFiles } = getRelatedFiles(rootNode, inputData, xmlParser);
+    expect(relatedFiles.length).to.equal(1);
+    expect(relatedFiles[0].path).to.equal('Types.xsd');
+    expect(missingRelatedFiles.length).to.equal(0);
+  });
+});
+
+describe('getRelatedFilesFromInput function ', function () {
+
+  it('should return result with all files present with 2 root files', function () {
+    const cCService = fs.readFileSync(M_I_SERVICE, 'utf8'),
+      cCData = fs.readFileSync(M_I_DATA, 'utf8'),
+      input = {
+        type: 'multiFile',
+        specificationVersion: '1.1',
+        rootFiles: [
+          {
+            path: '/CountingCategoryService.wsdl',
+            content: cCService
+          },
+          {
+            path: '/CountingCategoryService.wsdl',
+            content: cCService
+          }
+        ],
+        data: [
+          {
+            path: '/CountingCategoryService.wsdl',
+            content: cCService
+          },
+          {
+            path: '/CountingCategoryData.xsd',
+            content: cCData
+          }
+        ]
+      },
+      result = getRelatedFilesFromInput(input, xmlParser);
+    expect(result).to.deep.equal({
+      result: true,
+      output: {
+        type: 'relatedFiles',
+        specification: {
+          type: 'WSDL',
+          version: '1.1'
+        },
+        data: [
+          {
+            rootFile: {
+              path: '/CountingCategoryService.wsdl'
+            },
+            relatedFiles: [
+              {
+                path: '/CountingCategoryData.xsd'
+              }
+            ],
+            missingRelatedFiles: [
+              {
+                path: '/CommonData.xsd'
+              }
+            ]
+          },
+          {
+            rootFile: {
+              path: '/CountingCategoryService.wsdl'
+            },
+            relatedFiles: [
+              {
+                path: '/CountingCategoryData.xsd'
+              }
+            ],
+            missingRelatedFiles: [
+              {
+                path: '/CommonData.xsd'
+              }
+            ]
+          }
+        ]
+      }
+    });
+  });
+
+  it('should throw error when rootFiles is undefined', function () {
+    const cCService = fs.readFileSync(M_I_SERVICE, 'utf8'),
+      cCData = fs.readFileSync(M_I_DATA, 'utf8'),
+      input = {
+        type: 'multiFile',
+        specificationVersion: '1.1',
+        data: [
+          {
+            path: '/CountingCategoryService.wsdl',
+            content: cCService
+          },
+          {
+            path: '/CountingCategoryData.xsd',
+            content: cCData
+          }
+        ]
+      };
+    try {
+      getRelatedFilesFromInput(input, xmlParser);
+      expect.fail(null, null, 'We expected to throw an error');
+    }
+    catch (error) {
+      expect(error.message).to.equal('Input should have at least one root file');
+    }
+  });
+
+  it('should throw error when rootFiles is empty', function () {
+    const cCService = fs.readFileSync(M_I_SERVICE, 'utf8'),
+      cCData = fs.readFileSync(M_I_DATA, 'utf8'),
+      input = {
+        type: 'multiFile',
+        specificationVersion: '1.1',
+        rootFiles: [],
+        data: [
+          {
+            path: '/CountingCategoryService.wsdl',
+            content: cCService
+          },
+          {
+            path: '/CountingCategoryData.xsd',
+            content: cCData
+          }
+        ]
+      };
+    try {
+      getRelatedFilesFromInput(input, xmlParser);
+      expect.fail(null, null, 'We expected to throw an error');
+    }
+    catch (error) {
+      expect(error.message).to.equal('Input should have at least one root file');
+    }
+  });
+
+  it('should return only version 1.1 when 2.0 and 1.1 are present and 1.1 is selected', function () {
+    const cCService = fs.readFileSync(M_I_SERVICE, 'utf8'),
+      cCData = fs.readFileSync(M_I_DATA, 'utf8'),
+      services20 = fs.readFileSync(WIKI_2_0_SERVICE, 'utf8'),
+      input = {
+        type: 'multiFile',
+        specificationVersion: '1.1',
+        rootFiles: [
+          {
+            path: '/CountingCategoryService.wsdl',
+            content: cCService
+          },
+          {
+            path: '/wikipedia.wsdl',
+            content: services20
+          }
+        ],
+        data: [
+          {
+            path: '/CountingCategoryService.wsdl',
+            content: cCService
+          },
+          {
+            path: '/wikipedia.wsdl',
+            content: services20
+          },
+          {
+            path: '/CountingCategoryData.xsd',
+            content: cCData
+          }
+        ]
+      },
+      result = getRelatedFilesFromInput(input, xmlParser);
+    expect(result).to.deep.equal({
+      result: true,
+      output: {
+        type: 'relatedFiles',
+        specification: {
+          type: 'WSDL',
+          version: '1.1'
+        },
+        data: [
+          {
+            rootFile: {
+              path: '/CountingCategoryService.wsdl'
+            },
+            relatedFiles: [
+              {
+                path: '/CountingCategoryData.xsd'
+              }
+            ],
+            missingRelatedFiles: [
+              {
+                path: '/CommonData.xsd'
+              }
+            ]
+          }
+        ]
+      }
+    });
+  });
+
+  it('should return only version 2.0 when 2.0 and 1.1 are present and 2.0 is selected', function () {
+    const cCService = fs.readFileSync(M_I_SERVICE, 'utf8'),
+      services20 = fs.readFileSync(WIKI_2_0_SERVICE, 'utf8'),
+      types20 = fs.readFileSync(WIKI_2_0_TYPES, 'utf8'),
+      input = {
+        type: 'multiFile',
+        specificationVersion: '2.0',
+        rootFiles: [
+          {
+            path: '/CountingCategoryService.wsdl',
+            content: cCService
+          },
+          {
+            path: '/wikipedia.wsdl',
+            content: services20
+          }
+        ],
+        data: [
+          {
+            path: '/CountingCategoryService.wsdl',
+            content: cCService
+          },
+          {
+            path: '/wikipedia.wsdl',
+            content: services20
+          },
+          {
+            path: '/Types.xsd',
+            content: types20
+          }
+        ]
+      },
+      result = getRelatedFilesFromInput(input, xmlParser);
+    expect(result).to.deep.equal({
+      result: true,
+      output: {
+        type: 'relatedFiles',
+        specification: {
+          type: 'WSDL',
+          version: '2.0'
+        },
+        data: [
+          {
+            rootFile: {
+              path: '/wikipedia.wsdl'
+            },
+            relatedFiles: [
+              {
+                path: '/Types.xsd'
+              }
+            ],
+            missingRelatedFiles: [
+            ]
+          }
+        ]
+      }
+    });
   });
 
 });
