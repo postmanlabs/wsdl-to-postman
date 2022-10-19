d996febe0ba9e4cc5e9329123ae8b5fe83d4af38
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2022-10-19 11:44:23 -0500

    ref map scenarios
    ref map with wsdl elements solving

--- test/unit/bundle.test.js
+++ test/unit/bundle.test.js
@@ -4,6 +4,7 @@ const expect = require('chai').expect,
   Converter = require('../../index.js'),
   path = require('path-browserify'),
   COUNTING_FOLDER = '../data/separatedFiles/counting',
+  W3_FOLDER = '../data/separatedFiles/W3Example',
   INCLUDE_TAG = '../data/separatedFiles/includeTag',
   fs = require('fs'),
   _ = require('lodash'),
@@ -93,7 +94,6 @@ describe('Bundle api, bundle method', function() {
     ).to.equal(removeLineBreakTabsSpaces(expectedOutput));
   });
 
-
   it('Should return the bundle api output correctly with reference map', async function() {
     const serviceContent = fs.readFileSync(
         path.join(__dirname, COUNTING_FOLDER, '/CountingCategoryService.wsdl'),
@@ -148,6 +148,86 @@ describe('Bundle api, bundle method', function() {
         }),
         nodes = select(xpath, doc);
       expect(nodes).to.not.be.undefined;
+      expect(nodes.length).to.equal(1);
+    });
+  });
+
+  it('Should bundle with reference map pointing to schema and a WSDL portType', async function () {
+    const folderPath = path.join(__dirname, W3_FOLDER),
+      serviceContent = fs.readFileSync(
+        path.join(folderPath, '/stockquoteservice.wsdl'),
+        'utf-8'
+      ),
+      wsldParts = fs.readFileSync(
+        path.join(folderPath, '/stockquote.wsdl'),
+        'utf-8'
+      ),
+      typesContent = fs.readFileSync(
+        path.join(folderPath, '/stockquote.xsd'),
+        'utf-8'
+      ),
+      input = {
+        type: 'multiFile',
+        specificationVersion: '1.1',
+        rootFiles: [
+          {
+            path: '/stockquoteservice.wsdl'
+          }
+        ],
+        options: { includeReferenceMap: true },
+        data: [
+          {
+            path: '/stockquote.xsd',
+            content: typesContent
+          },
+          {
+            path: '/stockquoteservice.wsdl',
+            content: serviceContent
+          },
+          {
+            path: '/stockquote.wsdl',
+            content: wsldParts
+          }
+        ]
+      },
+      result = await Converter.bundle(input);
+    expect(result.result).to.be.true;
+    expect(result.output).to.be.an('object')
+      .with.keys(['data', 'type', 'specification']);
+    expect(result.output.specification).to.be.an('object')
+      .with.keys(['type', 'version']);
+    expect(result.output.data)
+      .to.have.length(1);
+    expect(result.output.data[0].rootFile.path)
+      .to.be.equal('/stockquoteservice.wsdl');
+    expect(result.output.data[0].rootFile.referenceMap['//definitions//portType[@name="StockQuotePortType"]'])
+      .to.deep.equal({
+        path: '/stockquote.wsdl',
+        type: 'inline'
+      });
+    expect(result.output.data[0].rootFile.referenceMap['//definitions//message[@name="GetLastTradePriceInput"]'])
+      .to.deep.equal({
+        path: '/stockquote.wsdl',
+        type: 'inline'
+      });
+    expect(result.output.data[0].rootFile.referenceMap['//definitions//message[@name="GetLastTradePriceOutput"]'])
+      .to.deep.equal({
+        path: '/stockquote.wsdl',
+        type: 'inline'
+      });
+    expect(result.output.data[0].rootFile.referenceMap['//definitions//types/schema[1]'])
+      .to.deep.equal({
+        path: '/stockquote.xsd',
+        type: 'inline'
+      });
+    _.keys(result.output.data[0].rootFile.referenceMap).forEach((xpath) => {
+      let doc = new xmldom().parseFromString(result.output.data[0].rootFile.bundledContent
+          .replace('<?xml version="1.0"?>', '')
+          .replace('xmlns="http://www.w3.org/2001/XMLSchema"', '')
+          .replace('xmlns="http://schemas.xmlsoap.org/wsdl/"', '')),
+        nodes = xpathTool.select(xpath, doc);
+      expect(nodes).to.not.be.undefined;
+      expect(nodes.length).to.equal(1);
     });
   });
 });
